41f759cb97dd3512df98e38adef679ef4959463b
Erick Lavoie <erick.lavoie@gmail.com>
2011-02-18 11:16:55 -0500

    HC: Added a test on current best representation on terry

--- posttraitement/hc.py
+++ posttraitement/hc.py
@@ -27,6 +27,7 @@ def gaussian(X, u, E):
 # k       : number of clusters at each step
 # fullcov :  True will use the covariance
 #            None will use the variance along each dimension independently (faster)
+# verbose :  True prints status messages as training progresses
 #
 # Output
 #         : 3-uple 
@@ -34,7 +35,7 @@ def gaussian(X, u, E):
 #          list of nb covariance/variance matrices
 #          array of nb prior probabilities
 #          with nb = (k**1 + k**2 + ... + k**step)
-def hc(dataset, max_step=5, k=2, fullcov=True):
+def hc(dataset, max_step=5, k=2, fullcov=True, verbose=False):
     # Nb of clusters to find 
     nb = kmnb(max_step, k)
     # Nb of features
@@ -46,12 +47,22 @@ def hc(dataset, max_step=5, k=2, fullcov=True):
     # Prior probabilities
     priors   = zeros((nb,))
 
+
+    nb_done = [0]
+    def progress(k): 
+        nb_done[0] += k
+        if verbose: 
+            print "Computed %i/%i clusters"%(nb_done[0], nb)
+
+
     def helper(dataset, step, base_idx, offset, prior):
         cs = kmeans(dataset, k)
         p = partition(dataset, cs)
 
         index = base_idx + k*offset
         means[index:index+k,:] = cs
+
+        progress(k)
 
         for i in range(k):
             ix = (p == i)
@@ -116,10 +127,11 @@ def partition(dataset, means):
 # priors : c x 1 matrix of prior probabilities for each cluster, optional
 # k      : int   if 0, no normalization is done, otherwise normalize as if
 #                hc was computed with k clusters at each step
+# verbose :  True prints status messages
 #
 # Output:
 #        : n x c matrix that contains the probabilities
-def probs(dataset, means, vars, priors=None, k=0):
+def probs(dataset, means, vars, priors=None, k=0, verbose=False):
     eps = finfo(float).eps
     Z = identity(vars[0].shape[0]) * (1/eps)
    
@@ -140,16 +152,21 @@ def probs(dataset, means, vars, priors=None, k=0):
     c = means.shape[0]
     ps = zeros((n,c))
 
-    if priors != None:
-        for i in xrange(c):
+    for i in xrange(c):
+        if priors != None:
             ps[:,i] = gaussian(dataset, means[i,:], vars[i]) * priors[i]
-    else:
-        for i in xrange(c):
+        else:
             ps[:,i] = gaussian(dataset, means[i,:], vars[i])
+
+        if verbose:
+            print "Computed %i/%i probabilities"%(i+1,c)
 
     # Normalize (Divide all computed posteriors 
     # by the sum of posteriors at the same level)
     if k != 0:
+        if verbose:
+            print "Normalizing"
+
         step = 1
         i = 0
         j = k
@@ -163,22 +180,86 @@ def probs(dataset, means, vars, priors=None, k=0):
 
     return ps
 
-if __name__ == "__main__":
-    dataset = array([[-4, 2],[-3, 2],[-4, 1],[-3, 1],[-4,-1],[-3,-1],[-4,-2],[-3,-2],\
-                     [ 3, 2],[ 4, 2],[ 3, 1],[ 4, 1],[ 3,-1],[ 4,-2],[ 3,-2],[ 4,-1]], dtype='float')
-    #dataset = random.rand(100000,200)
-   
+def test_terry():
+    dataset = "terry"
+    save_dir_submission = "./"
+
+    data = load("/data/lisa/exp/dauphiya/stackedterry/best_layer0/terry_valid.npy")
+
     print "Computing clusters"
     start = time.time()    
-    (means, vars, priors) = hc(dataset, 3, 2)
+    (means, vars, priors) = hc(data, 5, 2, verbose=True)
     end = time.time()
     print "Clusters computed in %i s"%(end-start)
 
     print "Computing probabilities"
     start = time.time()
-    ps = probs(dataset, means, vars, priors, k=2)
+    valid_rep1 = probs(data, means, vars, priors, k=2, verbose=True)
     end = time.time()
     print "Probabilities computed in %i s"%(end-start)
+
+    valid_rep2 = numpy.dot(valid_rep1,valid_rep1.T)
+
+    # write it in a .txt file
+    valid_rep1 = numpy.floor((valid_rep1 / valid_rep1.max())*999)
+    valid_rep2 = numpy.floor((valid_rep2 / valid_rep2.max())*999)
+
+    val1 = open(os.path.join(save_dir_submission, dataset + '_dl_valid.prepro'),'w')
+    val2 = open(os.path.join(save_dir_submission, dataset + '_sdl_valid.prepro'),'w')
+
+    vtxt1, vtxt2 = '', ''
+
+    for i in range(valid_rep1.shape[0]):
+        for j in range(valid_rep1.shape[0]):
+            vtxt2 += '%s '%int(valid_rep2[i,j])
+        for j in range(valid_rep1.shape[1]):
+            vtxt1 += '%s '%int(valid_rep1[i,j])
+        vtxt1 += '\n'
+        vtxt2 += '\n'
+    del valid_rep1, valid_rep2
+
+    val1.write(vtxt1)
+    val2.write(vtxt2)
+    val1.close()
+    val2.close()
+
+    print >> sys.stderr, "... done creating files"
+
+    os.system('zip %s %s %s'%(os.path.join(save_dir_submission, dataset+'_dl.zip'),
+        os.path.join(save_dir_submission, dataset+'_dl_valid.prepro'),
+        os.path.join(save_dir_submission, dataset+'_dl_final.prepro')))
+    os.system('zip %s %s %s'%(os.path.join(save_dir_submission, dataset+'_sdl.zip'),
+        os.path.join(save_dir_submission, dataset+'_sdl_valid.prepro'),
+        os.path.join(save_dir_submission, dataset+'_sdl_final.prepro')))
+
+    print >> sys.stderr, "... files compressed"
+
+    os.system('rm %s %s %s %s'%(
+        os.path.join(save_dir_submission, dataset+'_dl_valid.prepro'),
+        os.path.join(save_dir_submission, dataset+'_dl_final.prepro'),
+        os.path.join(save_dir_submission, dataset+'_sdl_valid.prepro'),
+        os.path.join(save_dir_submission, dataset+ '_sdl_final.prepro')))
+
+    print >> sys.stderr, "... useless files deleted"
+
+
+if __name__ == "__main__":
+    #dataset = array([[-4, 2],[-3, 2],[-4, 1],[-3, 1],[-4,-1],[-3,-1],[-4,-2],[-3,-2],\
+    #                 [ 3, 2],[ 4, 2],[ 3, 1],[ 4, 1],[ 3,-1],[ 4,-2],[ 3,-2],[ 4,-1]], dtype='float')
+    #dataset = random.rand(4096,1000)
+   
+    #print "Computing clusters"
+    #start = time.time()    
+    #(means, vars, priors) = hc(dataset, 3, 2, verbose=True)
+    #end = time.time()
+    #print "Clusters computed in %i s"%(end-start)
+
+    #print "Computing probabilities"
+    #start = time.time()
+    #ps = probs(dataset, means, vars, priors, k=2, verbose=True)
+    #end = time.time()
+    #print "Probabilities computed in %i s"%(end-start)
+    test_terry()
     
 
         
