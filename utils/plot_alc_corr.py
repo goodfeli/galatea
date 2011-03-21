14dc2fcca8c6c4a1c72deeb4f518baa31ad83b97
Erick Lavoie <erick.lavoie@gmail.com>
2011-03-21 12:17:33 -0400

    ALC: Added script to compute correlation between train alc and valid alc on ule

--- utils/plot_alc_corr.py
+++ utils/plot_alc_corr.py
@@ -8,13 +8,14 @@ import sys
 def corr_plot(data="alc_vs_vt_sylvester.log"):
     res = loadtxt(data)
     f = figure()
-    scatter(res[:,1], res[:,0])
-    title("Correlation between valid/test alc vs true alc for " + data)
-    xlabel("valid/test alc")
+    scatter(res[:,0], res[:,1])
+    title("Correlation between train alc and true alc for " + data)
+    xlabel("train alc")
     ylabel("true valid alc")
 
     for i in range(res.shape[0]):
-        annotate(str(int(res[i,2])), (res[i,1], res[i,0]))
+        print res[i,2]
+        annotate(str(int(res[i,2])), (res[i,0], res[i,1]))
 
     savefig(data[:-4])
 
@@ -24,6 +25,6 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         f = corr_plot(data=sys.argv[1])
     else:
-        print "Usage: python plot_corr.py <file>"
+        print "Usage: python plot_alc_corr.py <file>"
 
 
