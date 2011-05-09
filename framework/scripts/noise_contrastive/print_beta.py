3aefe68fad42fd95b0dc2346b8af2f3113d57232
Ian Goodfellow <goodfellow.ian@gmail.com>
2011-05-09 19:38:26 -0400

    fuck you git

--- framework/scripts/noise_contrastive/print_beta.py
+++
@@ -1,7 +0,0 @@
-from framework.utils import serial
-import sys
-
-for arg in sys.argv[1:]:
-    model = serial.load(arg)
-    print arg
-    print model.beta.get_value()
--- framework/scripts/noise_contrastive/test_dataset.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-from framework.datasets.cos_dataset import CosDataset
-
-d = CosDataset()
-
-x = d.get_batch_design(100)
-
-plt.scatter(x[:,0],x[:,1])
-plt.show()
