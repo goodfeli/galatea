5902096dba1ce731de38d62d8623b1aad3f43953
Ian Goodfellow <goodfellow.ian@gmail.com>
2011-08-11 16:30:08 -0400

    sphere preprocessed dataset: made arguments to GCN explicit, changed path to be in /data/lisatmp

--- datasets/make_cifar_2M_sphere.py
+++ datasets/make_cifar_2M_sphere.py
@@ -6,19 +6,21 @@ train = cifar10.CIFAR10(which_set="train")
 
 pipeline = preprocessing.Pipeline()
 pipeline.items.append(preprocessing.ExtractPatches(patch_shape=(8,8),num_patches=2000000))
-pipeline.items.append(preprocessing.GlobalContrastNormalization())
+pipeline.items.append(preprocessing.GlobalContrastNormalization(std_bias = 0.0, use_norm = 1.))
 
 test = cifar10.CIFAR10(which_set="test")
 
+
 train.apply_preprocessor(preprocessor = pipeline, can_fit = True)
 test.apply_preprocessor(preprocessor = pipeline, can_fit = False)
+
 
 
 train.use_design_loc('/data/lisatmp/goodfeli/cifar10_sphere_train_2M_design.npy')
 test.use_design_loc('/data/lisatmp/goodfeli/cifar10_sphere_test_2M_design.npy')
 
 
-serial.save('cifar10_sphere_train_2M.pkl',train)
-serial.save('cifar10_sphere_test_2M.pkl',test)
+serial.save('/data/lisatmp/goodfeli/cifar10_sphere_train_2M.pkl',train)
+serial.save('/data/lisatmp/goodfeli/cifar10_sphere_test_2M.pkl',test)
 
-
+train = serial.load('/data/lisatmp/goodfeli/cifar10_sphere_train_2M.pkl')
