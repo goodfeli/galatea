b58aef67a47007481e48b451097e5b227b8312dd
Ian Goodfellow <goodfellow.ian@gmail.com>
2011-09-16 10:03:44 -0400

    modified some config files# Please enter the commit message for your changes. Lines starting

--- s3c/s3c.py
+++ s3c/s3c.py
@@ -1523,8 +1523,7 @@ class VHS_Solve_M_Step(VHS_M_Step):
         #handle all step-size mangling on W   BEFORE COMPUTING B
         if model.constrain_W_norm:
             norms = T.sqrt(1e-8+T.sqr(new_W).sum(axis=0))
-            if model.monitor_norms:
-                learning_updates[model.debug_norms] = norms
+            learning_updates[model.debug_norms] = norms
             new_W = new_W / norms.dimshuffle('x',0)
         new_W = new_coeff * new_W + (one - new_coeff) * model.W
         dummy = { model.W :  new_W }
