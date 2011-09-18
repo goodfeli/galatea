fb9a62ebf4ce95875c9a443efccfa2c056c08362
Ian Goodfellow <goodfellow.ian@gmail.com>
2011-09-18 11:49:38 -0400

    copied feature_viewer to extract_features.py

--- s3c/extract_features.py
+++ s3c/extract_features.py
@@ -1,3 +1,4 @@
+import numpy as np
 from pylearn2.datasets.cifar10 import CIFAR10
 from theano import config
 from theano import tensor as T
@@ -9,22 +10,37 @@ from pylearn2.utils import serial
 from pylearn2.datasets.dense_design_matrix import DenseDesignMatrix, DefaultViewConverter
 from pylearn2.gui.patch_viewer import PatchViewer
 
-print 'loading dataset'
-dataset = CIFAR10(which_set = "train")
 
 batch_start = 0
-batch_size = 5
+batch_size = 15
+k = 15
 
+import sys
+model_path = sys.argv[1]
+
+print 'loading model'
+model = serial.load(model_path)
+
+print 'loading dataset'
+dataset = CIFAR10(which_set = "train")
 X = dataset.get_design_matrix()[batch_start:batch_start + batch_size,:]
 
-pv1 = make_viewer(X, is_color = True)
+size = np.sqrt(model.nvis/3)
+
+
+pv1 = make_viewer( (X-127.5)/127.5, is_color = True, rescale = False)
 dataset.set_design_matrix(X)
 
-patchifier = ExtractGridPatches( patch_shape = (8,8), patch_stride = (1,1) )
+patchifier = ExtractGridPatches( patch_shape = (size,size), patch_stride = (1,1) )
 
 
-
-pipeline = serial.load('${GOODFELI_TMP}/cifar10_preprocessed_pipeline_2M.pkl')
+if size == 8:
+    pipeline = serial.load('${GOODFELI_TMP}/cifar10_preprocessed_pipeline_2M.pkl')
+elif size ==6:
+    pipeline = serial.load('${GOODFELI_TMP}/cifar10_preprocessed_pipeline_2M_6x6.pkl')
+else:
+    print size
+    assert False
 
 assert isinstance(pipeline.items[0], ExtractPatches)
 pipeline.items[0] = patchifier
@@ -36,11 +52,6 @@ dataset.apply_preprocessor(pipeline, can_fit = False)
 X2 = dataset.get_design_matrix()
 
 
-import sys
-model_path = sys.argv[1]
-
-print 'loading model'
-model = serial.load(model_path)
 
 print 'defining features'
 V = T.matrix()
@@ -61,7 +72,8 @@ feat = f(X2)
 feat_dataset = DenseDesignMatrix(X = feat, view_converter = DefaultViewConverter([1, 1, feat.shape[1]] ) )
 
 print 'reassembling features'
-depatchifier = ReassembleGridPatches( orig_shape  = (25, 25), patch_shape=(1,1) )
+ns = 32 - size + 1
+depatchifier = ReassembleGridPatches( orig_shape  = (ns, ns), patch_shape=(1,1) )
 feat_dataset.apply_preprocessor(depatchifier)
 
 print 'making topological view'
@@ -69,18 +81,39 @@ topo_feat = feat_dataset.get_topological_view()
 assert topo_feat.shape[0] == X.shape[0]
 
 print 'assembling visualizer'
-k = 5
-pv3 = PatchViewer(grid_shape = (X.shape[0], k), patch_shape=(25,25), is_color= False)
-pv4 = PatchViewer(grid_shape = (k, 1), patch_shape = (8,8), is_color = True)
+
+n = np.ceil(np.sqrt(model.nhid))
+
+pv3 = PatchViewer(grid_shape = (X.shape[0], k), patch_shape=(ns,ns), is_color= False)
+pv4 = PatchViewer(grid_shape = (n,n), patch_shape = (size,size), is_color = True, pad = (7,7))
 
 idx = sorted(range(model.nhid), key = lambda l : -topo_feat[:,:,:,l].std() )
 
 W = model.W.get_value()
 
-call weights view and use that that
+weights_view = dataset.get_weights_view( W.T )
 
-for j in xrange(k):
-    pv4.add_patch(weights at idx[j], weights = True), rescale = True)
+p_act = 1. / (1. + np.exp(- model.bias_hid.get_value()))
+p_act /= p_act.max()
+
+mu_act = np.abs(model.mu.get_value())
+mu_act /= mu_act.max()
+mu_act += 0.5
+
+alpha_act = model.alpha.get_value()
+alpha_act /= alpha_act.max()
+
+for j in xrange(model.nhid):
+    cur_idx = idx[j]
+
+    cur_p_act = p_act[cur_idx]
+    cur_mu_act = mu_act[cur_idx]
+    cur_alpha_act = alpha_act[cur_idx]
+
+    activation = (cur_p_act, cur_mu_act, cur_alpha_act)
+
+    pv4.add_patch(weights_view[cur_idx,:], rescale = True,
+            activation = activation)
 
 for i in xrange(X.shape[0]):
     #indices of channels sorted in order of descending standard deviation on this example
@@ -91,7 +124,6 @@ for i in xrange(X.shape[0]):
 pv1.show()
 pv3.show()
 pv4.show()
-
 
 #pv2 = make_viewer(X2, is_color = True)
 #pv2.show()
