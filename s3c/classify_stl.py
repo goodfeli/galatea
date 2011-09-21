40db1a286e629b7ad85e291f801022b8c2a49a3b
Ian Goodfellow <goodfellow.ian@gmail.com>
2011-09-21 01:51:22 -0400

    completed classification pipeline

--- s3c/classify_stl.py
+++ s3c/classify_stl.py
@@ -1,3 +1,7 @@
+#TODO: support concatenating multiple datasets
+#TODO: support recursive refinement of C search
+#TODO: redo scopes so fewer dels are needed
+
 # allows to plot figures without X11 (i.e on cluster)
 import time
 import numpy as np
@@ -21,7 +25,36 @@ from scikits.learn.svm import LinearSVC, SVC
 
 from pylearn2.utils import serial
 
-def classify(fold_indices, train_X, train_y, X,y,  one_against_many=False ):
+def get_svm_type(C, one_against_many):
+    if one_against_many:
+        svm_type = LinearSVC(C=C)
+    else:
+        svm_type =  SVC(kernel='linear',C=C)
+    return svm_type
+
+def validate(train_X, train_y, fold_indices, C, one_against_many):
+    train_mask = np.zeros((5000,),dtype='uint8')
+    #Yes, Adam's site really does say to use the 1000 indices as the train,
+    #not the validation set
+    #The -1 is to convert from matlab indices
+    train_mask[fold_indices-1] = 1
+
+    this_fold_train_X = train_X[train_mask.astype(bool),:]
+    this_fold_train_y = train_y[train_mask.astype(bool)]
+    svm = get_svm_type(C, one_against_many).fit(this_fold_train_X,this_fold_train_y)
+    del this_fold_train_X
+    del this_fold_train_y
+
+    this_fold_valid_X = train_X[(1-train_mask).astype(bool),:]
+    y_pred = svm.predict(this_fold_valid_X)
+    del this_fold_valid_X
+    this_fold_valid_y = train_y[(1-train_mask).astype(bool)]
+
+    rval = (this_fold_valid_y == y_pred).mean()
+
+    return rval
+
+def train(fold_indices, train_X, train_y,  one_against_many=False ):
     """
     :param type: one of 'linear' or 'rbf'
     """
@@ -33,12 +66,6 @@ def classify(fold_indices, train_X, train_y, X,y,  one_against_many=False ):
 
     param_grid = { 'C': [.01, .02, .1, .2, 1, 5, 10, 50, 100], 'gamma': [0.] }
 
-    def get_svm_type(  my_C):
-        if one_against_many:
-            svm_type = LinearSVC(C=my_C)
-        else:
-            svm_type =  SVC(kernel='linear',C=my_C,)
-        return svm_type
 
     warnings.warn("""
     scikits GridSearchCV seemed to fail for bilinear rbm project so here we just
@@ -53,24 +80,8 @@ def classify(fold_indices, train_X, train_y, X,y,  one_against_many=False ):
         for i in xrange(fold_indices.shape[0]):
             print '  fold ',i
 
-            train_mask = np.zeros((5000,),dtype='uint8')
-            #Yes, Adam's site really does say to use the 1000 indices as the train,
-            #not the validation set
-            train_mask[fold_indices[i,:]-1] = 1
+            this_fold_acc = validate(train_X, train_y, fold_indices[i,:], C, one_against_many)
 
-            this_fold_train_X = train_X[train_mask.astype(bool),:]
-            this_fold_train_y = train_y[train_mask.astype(bool)]
-            svm = get_svm_type(C).fit(this_fold_train_X,this_fold_train_y)
-            del this_fold_train_X
-            del this_fold_train_y
-
-            this_fold_valid_X = train_X[(1-train_mask).astype(bool),:]
-            y_pred = svm.predict(this_fold_valid_X)
-            del this_fold_valid_X
-            this_fold_valid_y = train_y[(1-train_mask).astype(bool)]
-
-            this_fold_acc = (y_pred == this_fold_valid_y).mean()
-            del this_fold_valid_y
             print '   fold accuracy: %f' % (this_fold_acc,)
             acc += this_fold_acc / float(fold_indices.shape[0])
         print '  accuracy this C: %f' % (acc,)
@@ -83,54 +94,80 @@ def classify(fold_indices, train_X, train_y, X,y,  one_against_many=False ):
     print "Validation set accuracy: ",best_acc
 
     print "Training final svm"
-    final_svm = get_svm_type(best_C).fit(train_X, train_y)
+    final_svm = get_svm_type(best_C, one_against_many).fit(train_X, train_y)
 
+    return final_svm
+
+def test(model, X, y):
     print "Evaluating svm"
-    y_pred = final_svm.predict(X)
-    print classification_report(y, y_pred)#, labels=selected_target,
+    y_pred = model.predict(X)
+    try:
+        print "Accuracy ",(y == y_pred).mean()
+        print classification_report(y, y_pred)#, labels=selected_target,
                                 #class_names=category_names[selected_target])
 
-    print confusion_matrix(y, y_pred)#, labels=selected_target)
-
-    print "Accuracy ",(y == y_pred).mean()
+        print confusion_matrix(y, y_pred)#, labels=selected_target)
+    except:
+        print "something went wrong"
+        print 'y:'
+        print y
+        print 'y_pred:'
+        print y_pred
+        print 'extra info'
+        print type(y)
+        print type(y_pred)
+        print y.dtype
+        print y_pred.dtype
+        print y.shape
+        print y_pred.shape
 #
 
-def main(
-        data_path,
-        **kwargs):
 
+def get_labels_and_fold_indices():
     print 'loading entire stl-10 train set just to get the labels and folds'
     stl10 = serial.load("${PYLEARN2_DATA_PATH}/stl10/stl10_32x32/train.pkl")
     train_y = stl10.y
 
     fold_indices = stl10.fold_indices
-    del stl10
 
-    print 'loading data'
-    topo_view = serial.load(data_path)
+    return train_y, fold_indices
+
+def get_test_labels():
+    print 'loading entire stl-10 test set just to get the labels'
+    stl10 = serial.load("${PYLEARN2_DATA_PATH}/stl10/stl10_32x32/test.pkl")
+    return stl10.y
+
+def get_features(path):
+    if path.endswith('.npy'):
+        topo_view = np.load(path)
+    else:
+        topo_view = serial.load(path)
 
     view_converter = DefaultViewConverter(topo_view.shape[1:])
 
     print 'converting data'
-    train_X = view_converter.topo_view_to_design_mat(topo_view)
+    X = view_converter.topo_view_to_design_mat(topo_view)
 
-    warnings.warn('evaluating on train set')
+    return X
 
-    classify(fold_indices, train_X, train_y,  X = train_X, y = train_y,  **kwargs)
+def main(train_path,
+        test_path,
+        **kwargs):
 
+    train_y, fold_indices = get_labels_and_fold_indices()
 
-"""
-def experiment(state, channel):
-    Experiment script to launch jobman.
+    print 'loading training features'
+    train_X = get_features(train_path)
 
-    Usage:
-    jobman cmdline bilinear.scripts.basic.classify_bilinear.experiment TODO look at training script
-    dataset = make(state.dataset)
-    main(model_path = state.model_path,
-         which_set = state.which_set,
-         method = state.method,
-         dataset = dataset)
-    """
+    model = train(fold_indices, train_X, train_y, **kwargs)
+
+    del train_X
+    del train_y
+
+    y = get_test_labels()
+    X = get_features(test_path)
+
+    test(model,X,y)
 
 
 if __name__ == '__main__':
@@ -140,20 +177,17 @@ if __name__ == '__main__':
     """
 
     parser = OptionParser()
-    parser.add_option("-m", "--model",
-                  action="store", type="string", dest="model",
-                  help="path to pickled model file", metavar="MODEL",
-                  default='model.pkl')
-    parser.add_option("-d", "--data",
-                action="store", type="string", dest="data")
+    parser.add_option("-d", "--train",
+                action="store", type="string", dest="train")
+    parser.add_option("-t", "--test",
+                action="store", type="string", dest="test")
     parser.add_option("--one-against-one", action="store_false", dest="one_against_many", default=True,
                       help="use a one-against-one classifier rather than a one-against-many classifier")
 
 
     (options, args) = parser.parse_args()
-    assert os.path.exists(options.model)
 
-    main(data_path=options.data,
-         model_path=options.model,
+    main(train_path=options.train,
+         test_path=options.test,
          one_against_many = options.one_against_many,
     )
--- s3c/extract_features_stl.py
+++ s3c/extract_features_stl.py
@@ -1,134 +1,171 @@
+import os
+from pylearn2.config import yaml_parse
+import warnings
 import time
 import copy
 import numpy as np
-from pylearn2.datasets.cifar10 import CIFAR10
 from theano import config
 from theano import tensor as T
 #from theano.sandbox.neighbours import images2neibs
 from theano import function
 from pylearn2.datasets.preprocessing import ExtractPatches, ExtractGridPatches, ReassembleGridPatches
-from pylearn2.gui.patch_viewer import make_viewer
 from pylearn2.utils import serial
 from pylearn2.datasets.dense_design_matrix import DenseDesignMatrix, DefaultViewConverter
-
+import sys
 config.floatX = 'float32'
 
-batch_size = 5
+class FeatureExtractor:
+    def __init__(self, batch_size, model_path, pooling_region_counts,
+           save_paths, feature_type, which_set):
+        self.batch_size = batch_size
+        self.model_path = model_path
 
-import sys
-model_path = sys.argv[1]
+        assert len(pooling_region_counts) == len(save_paths)
 
-print 'loading model'
-model = serial.load(model_path)
-model.set_dtype('float32')
+        self.pooling_region_counts = pooling_region_counts
+        self.save_paths = save_paths
+        self.feature_type = feature_type
+        self.which_set = which_set
 
-print 'loading dataset'
-dataset = serial.load('${PYLEARN2_DATA_PATH}/stl10/stl10_32x32/train.pkl')
-full_X = dataset.get_design_matrix()
-dataset.X = None
-dataset.design_loc = None
-dataset.compress = False
+    def __call__(self):
+        model_path = self.model_path
+        batch_size = self.batch_size
+        feature_type = self.feature_type
+        pooling_region_counts = self.pooling_region_counts
 
-size = np.sqrt(model.nvis/3)
+        print 'loading model'
+        model = serial.load(model_path)
+        model.set_dtype('float32')
 
-patchifier = ExtractGridPatches( patch_shape = (size,size), patch_stride = (1,1) )
+        print 'loading dataset'
+        dataset = serial.load('${PYLEARN2_DATA_PATH}/stl10/stl10_32x32/'+self.which_set+'.pkl')
+        full_X = dataset.get_design_matrix()
+        num_examples = full_X.shape[0]
 
-if size ==6:
-    pipeline = serial.load('${PYLEARN2_DATA_PATH}/stl10/stl10_patches/preprocessor.pkl')
-else:
-    print size
-    assert False
+        if self.which_set == 'train':
+            assert num_examples == 5000
+        elif self.which_set == 'test':
+            assert num_examples == 8000
+        else:
+            assert False
 
-assert isinstance(pipeline.items[0], ExtractPatches)
-pipeline.items[0] = patchifier
+        dataset.X = None
+        dataset.design_loc = None
+        dataset.compress = False
+
+        size = np.sqrt(model.nvis/3)
+
+        patchifier = ExtractGridPatches( patch_shape = (size,size), patch_stride = (1,1) )
+
+        if size ==6:
+            pipeline = serial.load('${PYLEARN2_DATA_PATH}/stl10/stl10_patches/preprocessor.pkl')
+        else:
+            print size
+            assert False
+
+        assert isinstance(pipeline.items[0], ExtractPatches)
+        pipeline.items[0] = patchifier
 
 
-print 'defining features'
-V = T.matrix()
-model.make_Bwp()
-d = model.e_step.mean_field(V = V)
+        print 'defining features'
+        V = T.matrix()
+        model.make_Bwp()
+        d = model.e_step.mean_field(V = V)
 
-H = d['H']
-Mu1 = d['Mu1']
+        H = d['H']
+        Mu1 = d['Mu1']
 
-assert H.dtype == 'float32'
-assert Mu1.dtype == 'float32'
+        assert H.dtype == 'float32'
+        assert Mu1.dtype == 'float32'
 
-feat = H * Mu1
+        if self.feature_type == 'map_hs':
+            feat = (H > 0.5) * Mu1
+        elif self.feature_type == 'exp_hs':
+            feat = H * Mu1
+        else:
+            raise NotImplementedError()
 
-assert feat.dtype == 'float32'
-print 'compiling theano function'
-f = function([V],feat)
+        assert feat.dtype == 'float32'
+        print 'compiling theano function'
+        f = function([V],feat)
 
-topo_feat_var = T.TensorType(broadcastable = (False,False,False,False), dtype='float32')()
-region_features = function([topo_feat_var],
-        topo_feat_var.mean(axis=(1,2)) )
+        topo_feat_var = T.TensorType(broadcastable = (False,False,False,False), dtype='float32')()
+        region_features = function([topo_feat_var],
+                topo_feat_var.mean(axis=(1,2)) )
 
-def average_pool( stride ):
-    def point( p ):
-        return p * ns / stride
+        def average_pool( stride ):
+            def point( p ):
+                return p * ns / stride
 
-    rval = np.zeros( (topo_feat.shape[0], stride, stride, topo_feat.shape[3] ) , dtype = 'float32')
+            rval = np.zeros( (topo_feat.shape[0], stride, stride, topo_feat.shape[3] ) , dtype = 'float32')
 
-    for i in xrange(stride):
-        for j in xrange(stride):
-            rval[:,i,j,:] = region_features( topo_feat[:,point(i):point(i+1), point(j):point(j+1),:] )
+            for i in xrange(stride):
+                for j in xrange(stride):
+                    rval[:,i,j,:] = region_features( topo_feat[:,point(i):point(i+1), point(j):point(j+1),:] )
 
-    return rval
-assert full_X.shape[0] == 5000
+            return rval
 
-#out4 = np.zeros((5000,2,2,model.nhid),dtype='float32')
-#out9 = np.zeros((5000,3,3,model.nhid),dtype='float32')
-out16 = np.zeros((5000,4,4,model.nhid),dtype='float32')
+        outputs = [ np.zeros((num_examples,count,count,model.nhid),dtype='float32') for count in pooling_region_counts ]
 
-fd = DenseDesignMatrix(X = np.zeros((1,1),dtype='float32'), view_converter = DefaultViewConverter([1, 1, model.nhid] ) )
+        fd = DenseDesignMatrix(X = np.zeros((1,1),dtype='float32'), view_converter = DefaultViewConverter([1, 1, model.nhid] ) )
 
-ns = 32 - size + 1
-depatchifier = ReassembleGridPatches( orig_shape  = (ns, ns), patch_shape=(1,1) )
+        ns = 32 - size + 1
+        depatchifier = ReassembleGridPatches( orig_shape  = (ns, ns), patch_shape=(1,1) )
 
-for i in xrange(0,5000-batch_size+1,batch_size):
-    print i
-    t1 = time.time()
+        for i in xrange(0,num_examples-batch_size+1,batch_size):
+            print i
+            t1 = time.time()
 
-    d = copy.copy(dataset)
-    d.set_design_matrix(full_X[i:i+batch_size,:])
+            d = copy.copy(dataset)
+            d.set_design_matrix(full_X[i:i+batch_size,:])
 
-    t2 = time.time()
+            t2 = time.time()
 
-    #print '\tapplying preprocessor'
-    d.apply_preprocessor(pipeline, can_fit = False)
-    X2 = d.get_design_matrix()
+            #print '\tapplying preprocessor'
+            d.apply_preprocessor(pipeline, can_fit = False)
+            X2 = d.get_design_matrix()
 
-    t3 = time.time()
+            t3 = time.time()
 
-    #print '\trunning theano function'
-    feat = f(X2)
+            #print '\trunning theano function'
+            feat = f(X2)
 
-    t4 = time.time()
+            t4 = time.time()
 
-    assert feat.dtype == 'float32'
+            assert feat.dtype == 'float32'
 
-    feat_dataset = copy.copy(fd)
-    feat_dataset.set_design_matrix(feat)
+            feat_dataset = copy.copy(fd)
+            feat_dataset.set_design_matrix(feat)
 
-    #print '\treassembling features'
-    feat_dataset.apply_preprocessor(depatchifier)
+            #print '\treassembling features'
+            feat_dataset.apply_preprocessor(depatchifier)
 
-    #print '\tmaking topological view'
-    topo_feat = feat_dataset.get_topological_view()
-    assert topo_feat.shape[0] == batch_size
+            #print '\tmaking topological view'
+            topo_feat = feat_dataset.get_topological_view()
+            assert topo_feat.shape[0] == batch_size
 
-    t5 = time.time()
+            t5 = time.time()
 
-    #print '\taverage pooling 2x2'
-    #out4[i:i+batch_size,...] =  average_pool(2)
-    #out9[i:i+batch_size,...] =  average_pool(3)
-    out16[i:i+batch_size,...] =  average_pool(4)
+            #average pooling
+            for output, count in zip(outputs, pooling_region_counts):
+                output[i:i+batch_size,...] = average_pool(count)
 
-    t6 = time.time()
+            t6 = time.time()
 
-    print (t6-t1, t2-t1, t3-t2, t4-t3, t5-t4, t6-t5)
+            print (t6-t1, t2-t1, t3-t2, t4-t3, t5-t4, t6-t5)
 
-#serial.save('out4.pkl',out4)
-#serial.save('out9.pkl',out9)
-np.save('out16.npy',out16)
+        for output, save_path in zip(outputs, self.save_paths):
+            np.save(save_path,output)
+
+if __name__ == '__main__':
+    assert len(sys.argv) == 2
+    yaml_path = sys.argv[1]
+
+    assert yaml_path.endswith('.yaml')
+    val = yaml_path[0:-len('.yaml')]
+    os.environ['FEATURE_EXTRACTOR_YAML_PATH'] = val
+    os.putenv('FEATURE_EXTRACTOR_YAML_PATH',val)
+
+    extractor = yaml_parse.load_path(yaml_path)
+
+    extractor()
