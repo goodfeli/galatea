8fa03b2c184e3926dc8f01fb94189eb81a21532e
Ian Goodfellow <goodfellow.ian@gmail.com>
2011-09-25 14:54:34 -0400

    synch

--- s3c/dataset_instrument.py
+++ s3c/dataset_instrument.py
@@ -1,4 +1,4 @@
 from pylearn2.utils import serial
 
-train = serial.load('${PYLEARN_2_DATA_PATH}/stl10/stl10_32x32/train.npy')
+train = serial.load('${PYLEARN2_DATA_PATH}/stl10/stl10_32x32/train.npy')
 print train.mean()
