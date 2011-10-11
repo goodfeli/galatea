fab637d20b7c597412acfa61d164f0a00d30321a
Ian Goodfellow <goodfellow.ian@gmail.com>
2011-10-11 17:24:26 -0400

    concatenation

--- s3c/feature_loading.py
+++ s3c/feature_loading.py
@@ -3,6 +3,13 @@ import numpy as np
 from pylearn2.datasets.dense_design_matrix import DefaultViewConverter
 
 def get_features(path, split):
+    if path.find(',') != -1:
+        paths = path.split(',')
+        Xs = [ get_features(subpath, split) for subpath in paths ]
+        X = np.concatenate( Xs, axis = 1)
+        return X
+
+
     if path.endswith('.npy'):
         topo_view = np.load(path)
     else:
