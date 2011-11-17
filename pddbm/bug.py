df616f2724c30adf61b0f8470d5393bbe54791d6
Ian Goodfellow <goodfellow.ian@gmail.com>
2011-11-17 14:15:47 -0500

    made W a standalone shared, bug persists

--- pddbm/bug.py
+++ pddbm/bug.py
@@ -9,13 +9,16 @@ from theano import config
 from pylearn2.utils import make_name, as_floatX
 from theano import tensor as T
 from theano.printing import min_informative_str
+import numpy as np
+from pylearn2.utils import sharedX
+
+W = sharedX( np.zeros((400,400)))
+W.name = 'HARBINGER'
 
 class DebugInferenceProcedure(InferenceProcedure):
     def infer_H_hat(self, V, H_hat, G1_hat):
-        W = self.model.dbm.W[0]
         top_down = T.dot(G1_hat, W.T)
-        H = top_down
-        return H
+        return top_down
 
     def infer(self, V, return_history = False):
         s3c_e_step = self.s3c_e_step
@@ -41,9 +44,8 @@ class DebugInferenceProcedure(InferenceProcedure):
                 H_hat.name = 'new_H_hat_step_'+str(i)
             elif letter == 'g':
                 b = self.model.dbm.bias_hid[0]
-                W_below = self.model.dbm.W[0]
                 H_hat_below = H_hat
-                G_hat[number] = dbm_ip.infer_H_hat_one_sided(other_H_hat = H_hat_below, W = W_below, b = b)
+                G_hat[number] = dbm_ip.infer_H_hat_one_sided(other_H_hat = H_hat_below, W = W, b = b)
 
         return make_dict()
 
@@ -124,8 +126,8 @@ assert G_hat in constants
 
 
 a = T.dot(H_hat.T, G_hat[0])
-b = T.sum(dbm.W[0] * a)
+b = T.sum(W * a)
 
-test = T.grad(b, dbm.W[0], consider_constant = constants)
+test = T.grad(b, W, consider_constant = constants)
 print min_informative_str(test)
 assert False
