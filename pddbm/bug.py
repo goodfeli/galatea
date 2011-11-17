35b05680a3676151103870994af2643b714d28c7
Ian Goodfellow <goodfellow.ian@gmail.com>
2011-11-17 11:38:40 -0500

    cleanup did not get rid of bug

--- pddbm/bug.py
+++ pddbm/bug.py
@@ -11,7 +11,7 @@ from theano import tensor as T
 from theano.printing import min_informative_str
 
 class DebugInferenceProcedure(InferenceProcedure):
-    def infer_H_hat(self, V, H_hat, S_hat, G1_hat):
+    def infer_H_hat(self, V, H_hat, G1_hat):
         W = self.model.dbm.W[0]
         top_down = T.dot(G1_hat, W.T)
         H = T.nnet.sigmoid(top_down)
@@ -27,26 +27,23 @@ class DebugInferenceProcedure(InferenceProcedure):
 
         H_hat = s3c_e_step.init_H_hat(V)
         G_hat = dbm_ip.init_H_hat(H_hat)
-        S_hat = s3c_e_step.init_S_hat(V)
+        #S_hat = s3c_e_step.init_S_hat(V)
 
         H_hat.name = 'init_H_hat'
-        S_hat.name = 'init_S_hat'
+        #S_hat.name = 'init_S_hat'
 
         def make_dict():
 
             return {
                     'G_hat' : tuple(G_hat),
                     'H_hat' : H_hat,
-                    #'S_hat' : S_hat,
-                    #'var_s0_hat' : var_s0_hat,
-                    #'var_s1_hat': var_s1_hat,
                     }
 
         for i, step in enumerate(self.schedule):
             letter, number = step
 
             if letter == 'h':
-                H_hat = self.infer_H_hat(V = V, H_hat = H_hat, S_hat = S_hat, G1_hat = G_hat[0])
+                H_hat = self.infer_H_hat(V = V, H_hat = H_hat, G1_hat = G_hat[0])
                 H_hat.name = 'new_H_hat_step_'+str(i)
             elif letter == 'g':
                 b = self.model.dbm.bias_hid[0]
@@ -55,7 +52,6 @@ class DebugInferenceProcedure(InferenceProcedure):
                 G_hat[number] = dbm_ip.infer_H_hat_one_sided(other_H_hat = H_hat_below, W = W_below, b = b)
 
         return make_dict()
-
 
 class DebugDBM(DBM):
     def expected_energy(self, V_hat, H_hat):
@@ -82,53 +78,20 @@ class DebugPDDBM(PDDBM):
         super(DebugPDDBM,self).__init__(s3c,dbm,learning_rate,inference_procedure,print_interval,dbm_weight_decay)
 
     def make_learn_func(self, V):
-        """
-        V: a symbolic design matrix
-        """
-
         #run variational inference on the train set
         hidden_obs = self.inference_procedure.infer(V)
 
-        #make a restricted dictionary containing only vars s3c knows about
-        restricted_obs = {}
-        for key in hidden_obs:
-            if key != 'G_hat':
-                restricted_obs[key] = hidden_obs[key]
-
-
-        #request s3c sufficient statistics
-        needed_stats = \
-         S3C.expected_log_prob_v_given_hs_needed_stats().union(\
-         S3C.log_likelihood_s_given_h_needed_stats())
-         #stats = SufficientStatistics.from_observations(needed_stats = needed_stats,
-         #       V = V, **restricted_obs)
-
-        #don't backpropagate through inference
         obs_set = set(hidden_obs.values())
-        #stats_set = set(stats.d.values())
         constants = obs_set
-        #constants = obs_set.union(stats_set)
 
         G_hat = hidden_obs['G_hat']
         for i, G in enumerate(G_hat):
             G.name = 'final_G_hat[%d]' % (i,)
         H_hat = hidden_obs['H_hat']
         H_hat.name = 'final_H_hat'
-        #S_hat = hidden_obs['S_hat']
-        #S_hat.name = 'final_S_hat'
 
         assert H_hat in constants
         assert G_hat in constants
-        #assert S_hat in constants
-
-        #expected_log_prob_v_given_hs = self.s3c.expected_log_prob_v_given_hs(stats, \
-        #        H_hat = H_hat, S_hat = S_hat)
-        #assert len(expected_log_prob_v_given_hs.type.broadcastable) == 0
-
-
-        #expected_log_prob_s_given_h  = self.s3c.log_likelihood_s_given_h(stats)
-        #assert len(expected_log_prob_s_given_h.type.broadcastable) == 0
-
 
         expected_dbm_energy = self.dbm.expected_energy( V_hat = H_hat, H_hat = G_hat )
         assert len(expected_dbm_energy.type.broadcastable) == 0
@@ -144,69 +107,6 @@ class DebugPDDBM(PDDBM):
         print min_informative_str(test)
         assert False
         """)
-
-
-        #note: this is not the complete tractable part of the objective
-        #the objective also includes the entropy of Q, but we drop that since it is
-        #not a function of the parameters and we're not able to compute the true
-        #value of the objective function anyway
-        tractable_obj = expected_log_prob_v_given_hs + \
-                        expected_log_prob_s_given_h  - \
-                        expected_dbm_energy
-
-        assert len(tractable_obj.type.broadcastable) == 0
-
-
-        if self.dbm_weight_decay:
-
-            for i, t in enumerate(zip(self.dbm_weight_decay, self.dbm.W)):
-
-                coeff, W = t
-
-                coeff = as_floatX(coeff)
-                coeff = T.as_tensor_variable(coeff)
-                coeff.name = 'dbm_weight_decay_coeff_'+str(i)
-
-                tractable_obj = tractable_obj - coeff * T.mean(T.sqr(W))
-
-        assert len(tractable_obj.type.broadcastable) == 0
-
-        #take the gradient of the tractable part
-        params = self.get_params()
-        grads = T.grad(tractable_obj, params, consider_constant = constants)
-
-        #put gradients into convenient dictionary
-        params_to_grads = {}
-        for param, grad in zip(params, grads):
-            params_to_grads[param] = grad
-
-
-        print min_informative_str(params_to_grads[self.dbm.W[0]])
-        assert False
-
-        #add the approximate gradients
-        params_to_approx_grads = self.dbm.get_neg_phase_grads()
-
-        for param in params_to_approx_grads:
-            params_to_grads[param] = params_to_grads[param] + params_to_approx_grads[param]
-
-        learning_updates = self.get_param_updates(params_to_grads)
-
-        sampling_updates = self.dbm.get_sampling_updates()
-
-        for key in sampling_updates:
-            learning_updates[key] = sampling_updates[key]
-
-        self.censor_updates(learning_updates)
-
-        print "compiling function..."
-        t1 = time.time()
-        rval = function([V], updates = learning_updates)
-        t2 = time.time()
-        print "... compilation took "+str(t2-t1)+" seconds"
-        print "graph size: ",len(rval.maker.env.toposort())
-
-        return rval
 
 obj = DebugPDDBM(learning_rate = .01,
         dbm_weight_decay = [ 100. ],
