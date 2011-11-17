a799e4b6e0605b3be15f9f80d582080a39ba728b
Ian Goodfellow <goodfellow.ian@gmail.com>
2011-11-17 10:33:47 -0500

    made file exposing pddbm bug

---
+++ pddbm/bug.py
@@ -0,0 +1,51 @@
+from galatea.pddbm.pddbm import PDDBM
+from galatea.pddbm.pddbm import InferenceProcedure
+from pylearn2.models.dbm import DBM
+from pylearn2.models.rbm import RBM
+from pylearn2.models.s3c import S3C
+from pylearn2.models.s3c import Grad_M_Step
+
+
+obj = PDDBM(learning_rate = .01,
+        dbm_weight_decay = [ 100. ],
+        dbm =  DBM (
+                negative_chains = 100,
+                monitor_params = 1,
+                rbms = [ RBM(
+                                                  nvis = 400,
+                                                  nhid = 400,
+                                                  irange = .05,
+                                                  init_bias_vis = -3.
+                                                )
+                         ]
+        ),
+        s3c = S3C (
+               nvis = 108,
+               nhid = 400,
+               init_bias_hid = -3.,
+               max_bias_hid = -2.,
+               min_bias_hid = -8.,
+               irange  = .02,
+               constrain_W_norm = 1,
+               init_B  = 3.,
+               min_B   = .1,
+               max_B   = 1e6,
+               tied_B =  1,
+               init_alpha = 1.,
+               min_alpha = 1e-3,
+               max_alpha = 1e6,
+               init_mu =  0.,
+               monitor_params = [ 'B', 'p', 'alpha', 'mu', 'W' ],
+               m_step =  Grad_M_Step()
+               ),
+       inference_procedure = InferenceProcedure(
+                schedule = [ ['s',1.],   ['h',1.],   ['g',0],   ['h', 0.1], ['s',0.1],
+                             ['h',0.1], ['g',0],   ['h',0.1], ['s',0.1],  ['h',0.1],
+                             ['g',0],   ['h',0.1], ['s',0.1], ['h', 0.1], ['g',0],
+                             ['h',0.1], ['g',0],   ['h',0.1], ['s', 0.1], ['h',0.1] ],
+                monitor_kl = 0,
+                clip_reflections = 1,
+                rho = 0.5
+       ),
+       print_interval =  10000
+)
