961c0a93a1ccfc55df94819ff4ffc24e525222b9
Ian Goodfellow <goodfellow.ian@gmail.com>
2012-02-01 15:30:52 -0500

    pruning, leak persists

--- pddbm/gpu_mem_leak.py
+++ pddbm/gpu_mem_leak.py
@@ -20,33 +20,35 @@ import time
 import theano
 import gc
 
+dbm = DBM (
+        negative_chains = 100,
+        monitor_params = 1,
+        rbms = [ serial.load("/u/goodfeli/galatea/pddbm/config/stl/full/layer_2_from_C1_A.pkl") ]
+)
 
 grads = {}
 
 class PDDBM(Model):
 
     def __init__(self,
-            s3c,
-            dbm
-            ):
+            s3c):
+        global dbm
 
         super(PDDBM,self).__init__()
 
         self.s3c = s3c
         s3c.e_step.autonomous = False
 
-        self.dbm = dbm
-
         self.rng = np.random.RandomState([1,2,3])
 
-        self.s3c.bias_hid = self.dbm.bias_vis
+        self.s3c.bias_hid = dbm.bias_vis
 
         self.nvis = s3c.nvis
 
 
-        self.num_g = len(self.dbm.W)
+        self.num_g = len(dbm.W)
 
-        self.dbm.redo_everything()
+        dbm.redo_everything()
 
 
         for param in self.get_params():
@@ -54,7 +56,7 @@ class PDDBM(Model):
 
         self.test_batch_size = 2
 
-        params_to_approx_grads = self.dbm.get_neg_phase_grads()
+        params_to_approx_grads = dbm.get_neg_phase_grads()
 
         updates = {}
 
@@ -64,27 +66,20 @@ class PDDBM(Model):
             else:
                 updates[grads[param]] = T.zeros_like(param)
 
-        sampling_updates = self.dbm.get_sampling_updates()
+        sampling_updates = dbm.get_sampling_updates()
 
         for key in sampling_updates:
             assert key not in updates
             updates[key] = sampling_updates[key]
 
-        print 'compiling reset grad func'
         global f
         f = function([], updates = updates)
 
     def get_params(self):
-        return list(set(self.s3c.get_params()).union(set(self.dbm.get_params())))
+        return list(set(self.s3c.get_params()).union(set(dbm.get_params())))
 
 model = PDDBM(
-        dbm = DBM (
-                negative_chains = 100,
-                monitor_params = 1,
-                rbms = [ serial.load("/u/goodfeli/galatea/pddbm/config/stl/full/layer_2_from_C1_A.pkl") ]
-        ),
-        s3c =  serial.load("/u/goodfeli/galatea/pddbm/config/stl/full/layer_1_C1.pkl"),
-)
+        s3c =  serial.load("/u/goodfeli/galatea/pddbm/config/stl/full/layer_1_C1.pkl"))
 
 before =  theano.sandbox.cuda.cuda_ndarray.cuda_ndarray.mem_info()
 f()
