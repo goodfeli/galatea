c0a80dacde940421a63f61b61fe72074704660e7
Ian Goodfellow <goodfellow.ian@gmail.com>
2012-02-02 12:43:06 -0500

    updates to gpu_mem_leak

--- pddbm/gpu_mem_leak.py
+++ pddbm/gpu_mem_leak.py
@@ -6,9 +6,30 @@ from theano import function
 import theano
 import gc
 
-W = sharedX(np.zeros((399,219)))
+s = [1,2]
+before =  theano.sandbox.cuda.cuda_ndarray.cuda_ndarray.mem_info()
+W = sharedX(np.zeros((s[0],s[1])))
+gc.collect()
+gc.collect()
+gc.collect()
+after =  theano.sandbox.cuda.cuda_ndarray.cuda_ndarray.mem_info()
+diff = before[0] - after[0]
+expected_diff = s[0]*s[1]*4
 
-grad  = sharedX(np.zeros(W.get_value().shape))
+if diff > expected_diff:
+    print "W uses ",str(float(diff)/float(expected_diff))," times more memory than needed."
+    print "(",str(float(diff-expected_diff)/(1024. ** 2))," megabytes)"
+
+grad  =sharedX(np.zeros(W.get_value().shape))
+gc.collect()
+gc.collect()
+gc.collect()
+after_after = theano.sandbox.cuda.cuda_ndarray.cuda_ndarray.mem_info()
+diff = after_after[0] - after[0]
+
+if diff > expected_diff:
+    print "grad uses ",str(float(diff)/float(expected_diff))," times more memory than needed."
+
 
 updates = { grad : W}
 
