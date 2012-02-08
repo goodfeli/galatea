aee1956ccd8dc236cdb0086bba9e06791b4c3369
Ian Goodfellow <goodfellow.ian@gmail.com>
2012-02-08 16:21:03 -0500

    made sc learning support more options

--- s3c/sc_vq_demo/learn_cifar100_sc.m
+++ s3c/sc_vq_demo/learn_cifar100_sc.m
@@ -1,5 +1,18 @@
 
-function learn_cifar100_sc(lambda, save_path)
+function learn_cifar100_sc(lambda, save_path, iters, batch_size, new_coeff)
+
+	if nargin < 3
+		iters = 10
+	end
+
+	if nargin < 4
+		batch_size = 100000
+	end
+
+	if nargin < 5
+		new_coeff = .1
+	end
+
 	CIFAR100_PATCHES='/data/lisa/data/cifar100/cifar100_patches/data.mat';
 
 	%%%%% Configuration
@@ -12,7 +25,7 @@ function learn_cifar100_sc(lambda, save_path)
 	%patches = patches(1:100000,:);
 	%fprintf(1,'warning: shrinking size of dataset\n')
 
-	dictionary = minibatch_sc(patches, numBases, 10, lambda, 100000, .1);
+	dictionary = minibatch_sc(patches, numBases, iters, lambda, batch_size, new_coeff);
 
 	save(save_path, 'dictionary');
 end
