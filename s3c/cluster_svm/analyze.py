221ae0d0912450143835832d37b717757e615676
Ian Goodfellow <goodfell@briaree1.rqchp.qc.ca>
2012-02-08 17:21:05 -0500

    made cluster cross validation script

---
+++ s3c/cluster_svm/analyze.py
@@ -0,0 +1,60 @@
+#usage: python analyze.py <directory created by launch_workers>
+#prints out validation error for each value of C, with best printed last
+
+import sys
+
+ignore, target = sys.argv
+
+import os
+files = os.listdir(target)
+
+
+C_to_results = {}
+
+for name in files:
+
+    path = target + '/' + name
+
+    f = open(path,'r')
+
+    lines = f.readlines()
+
+    f.close()
+
+    header = 'C\tfold\tvalidation accuracy\n'
+
+    assert lines[0] == header
+    assert len(lines) == 2
+
+    C_str, fold_str, acc_str = lines[1].split()
+
+    C = float(C_str)
+    fold = int(fold_str)
+    acc = float(acc_str)
+
+
+    if C not in C_to_results:
+        C_to_results[C] = []
+
+    C_to_results[C].append(acc)
+
+num_folds = len(C_to_results.values()[0])
+
+best_acc = -1.
+
+for C in C_to_results:
+    results = C_to_results[C]
+
+    assert len(results) == num_folds
+
+    result = sum(results) / float(num_folds)
+
+    print '%(C)f: %(result)f' % locals()
+
+    if result > best_acc:
+        best_acc = result
+        best_C = C
+
+print '%d-fold valid acc: ' % num_folds,best_acc
+print 'best C: ',best_C
+
---
+++ s3c/cluster_svm/launch_workers.py
@@ -0,0 +1,30 @@
+#python launch_workers.py training_set.npy
+
+import sys
+import os
+from pylearn2.utils import serial
+
+assert len(sys.argv) == 2
+train_file = sys.argv[1]
+assert train_file.endswith('.npy')
+
+pieces = train_file.split('.npy')
+assert len(pieces) == 2
+
+results_dir = pieces[0]
+serial.mkdir(results_dir)
+
+command = 'jobdispatch --whitespace --mem=18G python /RQusagers/goodfell/galatea/s3c/cluster_svm/fold_point_worker.py --dataset cifar100 --train '
+command += train_file
+command += ' "{{'
+
+options = []
+for C in [.05,.1,.15,.2,.5,1]:
+    for fold in xrange(5):
+        options.append( '--fold %(fold)d -C %(C)f --out %(results_dir)s/%(fold)d_%(C)f.txt' % locals() )
+
+command += ','.join(options)
+command += '}}"'
+
+os.system(command)
+
