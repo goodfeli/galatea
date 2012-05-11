ef97293c4a8349adff771d2688a5f308afa6b0ca
Ian Goodfellow <goodfellow.ian@gmail.com>
2012-05-11 11:31:20 -0400

    more detail to show_samples

--- pddbm/show_samples.py
+++ pddbm/show_samples.py
@@ -37,38 +37,48 @@ model.dbm.redo_everything() #this just redoes the chains
 hidden_obs = model.inference_procedure.infer(sharedX(init_examples))
 
 from theano import function
-outputs = [ hidden_obs['H_hat'] ]
+from theano.tensor.shared_randomstreams import RandomStreams
+from pylearn2.models.dbm import Sampler
+theano_rng = RandomStreams(17)
+sample_from = Sampler(theano_rng)
+
+outputs = [ sample_from(hidden_obs['H_hat']) ]
 for G_hat in hidden_obs['G_hat']:
-    outputs.append(G_hat)
+    outputs.append( sample_from(G_hat) )
+outputs.append( hidden_obs['S_hat'])
 init_chain_hid = function([], outputs)()
 
 model.dbm.V_chains = sharedX(init_chain_hid[0])
-model.dbm.H_chains = [ sharedX(init_chain_elem) for init_chain_elem in init_chain_hid[1:] ]
+model.dbm.H_chains = [ sharedX(init_chain_elem) for init_chain_elem in init_chain_hid[1:-1] ]
+S_hat = init_chain_hid[-1]
 
-from theano.sandbox.rng_mrg import MRG_RandomStreams as RandomStreams
-theano_rng = RandomStreams(42)
 assert hasattr(model.dbm,'V_chains') and model.dbm.V_chains is not None
 design_examples_var = model.s3c.random_design_matrix(batch_size = rows * cols,
         theano_rng = theano_rng, H_sample = model.dbm.V_chains)
 print 'compiling sampling function'
-f = function([],design_examples_var, updates = model.dbm.get_sampling_updates())
+f = function([],design_examples_var)
+g = function([],model.s3c.random_design_matrix(batch_size = rows * cols,
+            theano_rng = theano_rng, H_sample = model.dbm.V_chains, S_sample = S_hat))
+sample = function([], updates = model.dbm.get_sampling_updates())
 
 
-print 'init_examples later',(init_examples.min(),init_examples.mean(),init_examples.max())
 examples = dataset.get_topological_view(init_examples)
-print 'examples ',(examples.min(),examples.mean(),examples.max())
 assert len(examples.shape) == 4
 is_color = examples.shape[3] == 3
 pv = patch_viewer.PatchViewer( (rows, cols), examples.shape[1:3], is_color = is_color)
+
+first = True
 
 while True:
 
     assert not np.any(np.isnan(examples))
     assert not np.any(np.isinf(examples))
 
+    if first:
+        print 'Showing seed examples...'
+
     examples = dataset.adjust_for_viewer(examples)
 
-    print 'examples scaled ',(examples.min(),examples.mean(),examples.max())
     for i in xrange(rows*cols):
         patch = examples[i,:,:,:]
         assert not np.any(np.isnan(patch))
@@ -81,12 +91,26 @@ while True:
     x = raw_input()
     if x == 'q':
         quit()
-    try:
-        reps = int(x)
-    except:
-        reps = 1
-    print 'one step of sampling...'
 
-    for i in xrange(reps):
+    if first:
+        first = False
+        second = True
+        print 'Showing sample from posterior, using S_hat from inference for s'
+
+        design_examples = g()
+    else:
+        if second:
+            second = False
+            print 'Showing sample from posterior'
+        else:
+            try:
+                reps = int(x)
+            except:
+                reps = 1
+            print reps,'step(s) of sampling...'
+
+            for i in xrange(reps):
+                sample()
         design_examples = f()
     examples = dataset.get_topological_view(design_examples)
+
