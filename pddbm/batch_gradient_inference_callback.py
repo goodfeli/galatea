68d781e6453d861872aea6c34d8c3428fc7672a3
Ian Goodfellow <goodfellow.ian@gmail.com>
2012-05-29 11:32:45 -0400

    added try/catch block so kl-monitoring callback can't bring the job down

--- pddbm/batch_gradient_inference_callback.py
+++ pddbm/batch_gradient_inference_callback.py
@@ -1,5 +1,6 @@
 from pylearn2.training_callbacks.training_callback import TrainingCallback
 from galatea.pddbm.batch_gradient_inference import BatchGradientInference
+import numpy as np
 
 class BatchGradientInferenceCallback(TrainingCallback):
 
@@ -8,18 +9,23 @@ class BatchGradientInferenceCallback(TrainingCallback):
 
     def __call__(self, model, dataset, algorithm):
 
-        if self.tester is None:
-            self.tester = BatchGradientInference(model)
-            self.X, self.Y = dataset.get_batch_design(algorithm.batch_size, include_labels = True)
-            if not self.tester.has_labels:
-                self.Y = None
-            model.kl_fail_log = []
+        try:
+            if self.tester is None:
+                self.tester = BatchGradientInference(model)
+                self.X, self.Y = dataset.get_batch_design(algorithm.batch_size, include_labels = True)
+                if not self.tester.has_labels:
+                    self.Y = None
+                model.kl_fail_log = []
 
-        results = self.tester.run_inference(self.X, self.Y)
+            results = self.tester.run_inference(self.X, self.Y)
 
-        diff = results['orig_kl'] - results['kl']
+            diff = results['orig_kl'] - results['kl']
 
-        print 'kl failure amount: ',diff
+            print 'kl failure amount: ',diff
+        except Exception, e:
+            print "BatchGradientInferenceCallback failed "
+            print e
+            diff = np.nan
 
         model.kl_fail_log.append((model.monitor.get_examples_seen(), diff))
 
