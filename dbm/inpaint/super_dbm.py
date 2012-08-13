6ab1977abd3e96e46219cba0f7175e696f835ca9
Ian Goodfellow <goodfellow.ian@gmail.com>
2012-08-13 13:48:19 -0400

    added mean field and softmax layer to super dbm

--- dbm/inpaint/super_dbm.py
+++ dbm/inpaint/super_dbm.py
@@ -66,10 +66,7 @@ class SuperDBM(Model):
             raise NotImplementedError()
 
     def get_weights_topo(self):
-        if len(self.hidden_layers) == 1:
-            return self.hidden_layers[0].get_weights_topo()
-        else:
-            raise NotImplementedError()
+        return self.hidden_layers[0].get_weights_topo()
 
     def do_inpainting(self, V, drop_mask, return_history = False):
 
@@ -135,11 +132,83 @@ class SuperDBM(Model):
         if return_history:
             return history
         else:
-            return V_hat
+             return V_hat
+
+    def mf(self, V, return_history = False):
+
+
+        H_hat = []
+        for i in xrange(0,len(self.hidden_layers)-1):
+            #do double weights update for_layer_i
+            if i == 0:
+                H_hat.append(self.hidden_layers[i].mf_update(
+                    state_above = None,
+                    double_weights = True,
+                    state_below = self.visible_layer.upward_state(V),
+                    iter_name = '0'))
+            else:
+                H_hat.append(self.hidden_layers[i].mf_update(
+                    state_above = None,
+                    double_weights = True,
+                    state_below = self.hidden_layers[i-1].upward_state(H_hat[i-1]),
+                    iter_name = '0'))
+        #last layer does not need its weights doubled, even on the first pass
+        if len(self.hidden_layers) > 1:
+            H_hat.append( self.hidden_layers[-1].mf_update(
+                state_above = None,
+                state_below = self.hidden_layers[-2].upward_state(H_hat[-1])))
+        else:
+            H_hat.append( self.hidden_layers[-1].mf_update(
+                state_above = None,
+                state_below = self.visible_layer.upward_state(V)))
+
+        history = [ H_hat ]
+
+        #we only need recurrent inference if there are multiple layers
+        if len(H_hat) > 1:
+            for i in xrange(self.niter-1):
+                for j in xrange(0,len(H_hat),2):
+                    if j == 0:
+                        state_below = self.visible_layer.upward_state(V)
+                    else:
+                        state_below = self.hidden_layers[j-1].upward_state(H_hat[j-1])
+                    if j == len(H_hat) - 1:
+                        state_above = None
+                    else:
+                        state_above = self.hidden_layers[j+1].downward_state(H_hat[j+1])
+                    H_hat[j] = self.hidden_layers[j].mf_update(
+                            state_below = state_below,
+                            state_above = state_above)
+
+                for j in xrange(1,len(H_hat),2):
+                    layer_below = self.hidden_layers[j-1].upward_state(H_hat[j-1])
+                    if j == len(H_hat) - 1:
+                        state_above = None
+                    else:
+                        state_above = self.hidden_layers[j+1].downward_state(H_hat[j+1])
+                    H_hat[j] = self.hidden_layers[j].mf_update(
+                            state_below = state_below,
+                            state_above = state_above)
+                    #end ifelse
+                #end for j
+                history.append(H_hat)
+            #end for i
+
+        if return_history:
+            return history
+        else:
+            return H_hat
 
 
 class SuperDBM_Layer(Model):
-    pass
+
+    def upward_state(self, total_state):
+        return total_state
+
+class SuperDBM_HidLayer(SuperDBM_Layer):
+
+    def downward_state(self, total_state):
+        return total_state
 
 class GaussianConvolutionalVisLayer(SuperDBM_Layer):
     def __init__(self,
@@ -153,6 +222,7 @@ class GaussianConvolutionalVisLayer(SuperDBM_Layer):
         del self.self
 
         self.space = Conv2DSpace(shape = [rows,cols], nchannels = channels)
+        self.input_space = self.space
         self.beta = sharedX( self.space.get_origin() + init_beta,name = 'beta')
         self.mu = sharedX( self.space.get_origin() + init_mu, name = 'mu')
 
@@ -165,7 +235,7 @@ class GaussianConvolutionalVisLayer(SuperDBM_Layer):
 
     def init_inpainting_state(self, V, drop_mask):
 
-        for Vv, drop_mask_v in get_debug_values(V, drop_mask):
+        """for Vv, drop_mask_v in get_debug_values(V, drop_mask):
             assert Vv.ndim == 4
             assert drop_mask_v.ndim in [3,4]
             for i in xrange(drop_mask.ndim):
@@ -173,6 +243,7 @@ class GaussianConvolutionalVisLayer(SuperDBM_Layer):
                     print Vv.shape
                     print drop_mask_v.shape
                     assert False
+        """
 
         masked_mu = self.mu * drop_mask
         masked_V  = V  * (1-drop_mask)
@@ -181,7 +252,10 @@ class GaussianConvolutionalVisLayer(SuperDBM_Layer):
 
     def inpaint_update(self, state_above, layer_above, drop_mask, V):
 
-        z = layer_above.downward_message(state_above) + self.mu
+        msg = layer_above.downward_message(state_above)
+        mu = self.mu
+
+        z = msg + mu
 
         rval = drop_mask * z + (1-drop_mask) * V
 
@@ -189,7 +263,9 @@ class GaussianConvolutionalVisLayer(SuperDBM_Layer):
 
     def recons_cost(self, V, V_hat, drop_mask = None):
 
+        assert V.ndim == V_hat.ndim
         unmasked_cost = 0.5 * self.beta * T.sqr(V-V_hat) - 0.5*T.log(self.beta / (2*np.pi))
+        assert unmasked_cost.ndim == V_hat.ndim
 
         if drop_mask is None:
             masked_cost = unmasked_cost
@@ -203,7 +279,7 @@ class GaussianConvolutionalVisLayer(SuperDBM_Layer):
         upward_state = V * self.beta
         return upward_state
 
-class ConvMaxPool(SuperDBM_Layer):
+class ConvMaxPool(SuperDBM_HidLayer):
     def __init__(self,
             output_channels,
             kernel_rows,
@@ -216,7 +292,7 @@ class ConvMaxPool(SuperDBM_Layer):
         self.__dict__.update(locals())
         del self.self
 
-        self.b = sharedX( np.zeros((output_channels,)), name = layer_name + '_b')
+        self.b = sharedX( np.zeros((output_channels,)) + init_bias, name = layer_name + '_b')
 
     def set_input_space(self, space):
         assert isinstance(space, Conv2DSpace)
@@ -231,12 +307,21 @@ class ConvMaxPool(SuperDBM_Layer):
         assert self.h_cols % self.pool_cols == 0
 
         self.h_space = Conv2DSpace( shape = (self.h_rows, self.h_cols), nchannels = self.output_channels)
+        self.output_space = Conv2DSpace( shape = (self.h_rows / self.pool_rows,
+                                                self.h_cols / self.pool_cols),
+                                                nchannels = self.output_channels)
 
         self.transformer = make_random_conv2D(self.irange, input_space = space,
                 output_space = self.h_space, kernel_shape = (self.kernel_rows, self.kernel_cols),
                 batch_size = self.dbm.batch_size)
+        self.transformer._filters.name = self.layer_name + '_W'
+        W ,= self.transformer.get_params()
+        assert W.name is not None
 
     def get_params(self):
+        assert self.b.name is not None
+        W ,= self.transformer.get_params()
+        assert W.name is not None
         return self.transformer.get_params().union([self.b])
 
     def upward_state(self, total_state):
@@ -247,11 +332,15 @@ class ConvMaxPool(SuperDBM_Layer):
         p,h = total_state
         return h
 
-    def mf_update(self, state_below, state_above):
+    def mf_update(self, state_below, state_above, double_weights = False, iter_name = None):
         if state_above is not None:
             raise NotImplementedError()
         assert hasattr(state_below,'ndim') and state_below.ndim == 4
+        if double_weights:
+            state_below = 2. * state_below
+            state_below.name = self.layer_name + '_'+iter_name + '_2state'
         z = self.transformer.lmul(state_below) + self.b
+        z.name = self.layer_name + '_' + iter_name + '_z'
         p,h = max_pool(z, (self.pool_rows, self.pool_cols))
 
         return p, h
@@ -268,5 +357,38 @@ class ConvMaxPool(SuperDBM_Layer):
 
         return np.transpose(raw,(outp,rows,cols,inp))
 
+class Softmax(SuperDBM_HidLayer):
+    def __init__(self, n_classes, irange):
+        self.__dict__.update(locals())
+        del self.self
+
+        self.output_space = VectorSpace(n_classes)
+        self.b = sharedX( np.zeros((n_classes,)), name = 'softmax_b')
+
+    def set_input_space(self, space):
+        self.input_space = space
+
+        if isinstance(space, Conv2DSpace):
+            self.input_dim = space.shape[0] * space.shape[1] * space.nchannels
+        else:
+            raise NotImplementedError()
+
+        rng = np.random.RandomState([2012,07,25])
+
+        self.W = sharedX( rng.uniform(-self.irange,self.irange, (self.input_dim,self.n_classes)), 'softmax_W' )
+
+        self._params = [ self.b, self.W ]
+
+    def mf_update(self, state_below, state_above):
+        if state_above is not None:
+            raise NotImplementedError()
+
+        state_below = state_below.reshape( (self.dbm.batch_size, self.input_dim) )
+
+        assert self.W.ndim == 2
+        return T.nnet.softmax(T.dot(state_below,self.W)+self.b)
+
+    def downward_message(self, downward_state):
+        return T.dot(downward_state, self.W.T)
 
 
