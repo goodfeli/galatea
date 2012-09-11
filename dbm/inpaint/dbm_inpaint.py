9cffb623592e23c432ab958ee155f413dfdecae9
Ian Goodfellow <goodfellow.ian@gmail.com>
2012-09-11 16:00:14 -0400

    added RBM Bad Variational

--- dbm/inpaint/dbm_inpaint.py
+++ dbm/inpaint/dbm_inpaint.py
@@ -269,6 +269,345 @@ class DBM_Inpaint_Binary(UnsupervisedCost):
 
         return ave_cost
 
+class RBM_Inpaint_Binary(UnsupervisedCost):
+    def __init__(self,
+                    n_iter,
+                    mask_gen = None,
+                    weight_decay = None,
+                    balance = False,
+                    reweight = True,
+                    reweight_correctly = False,
+                    h_target = None,
+                    h_penalty = None,
+                    ):
+        self.__dict__.update(locals())
+        del self.self
+        assert not (reweight and reweight_correctly)
+
+
+    def get_monitoring_channels(self, model, X, Y = None, drop_mask = None):
+
+        d = self(model, X = X, drop_mask = drop_mask, return_locals = True)
+
+        H_hat = d['H_hat']
+
+        h = H_hat.mean(axis=0)
+
+        rval = {}
+
+        rval['h_min'] = h.min()
+        rval['h_mean'] = h.mean()
+        rval['h_max'] = h.max()
+
+        h_max = H_hat.max(axis=0)
+        h_min = H_hat.min(axis=0)
+
+        h_range = h_max - h_min
+
+        rval['h_range_min'] = h_range.min()
+        rval['h_range_mean'] = h_range.mean()
+        rval['h_range_max'] = h_range.max()
+
+        return rval
+
+    def __call__(self, model, X, drop_mask = None, return_locals = False):
+
+        assert not hasattr(model, 'beta') # this is only for binary for now
+        if not hasattr(model,'cost'):
+            model.cost = self
+        if not hasattr(model,'mask_gen'):
+            model.mask_gen = self.mask_gen
+
+        dbm = model
+        assert len(dbm.rbms) == 1
+
+        ip = dbm.inference_procedure
+
+        if drop_mask is None:
+            drop_mask = self.mask_gen(X)
+        else:
+            assert self.mask_gen is None
+
+
+        vhW = dbm.W[0]
+
+        X_hat = X * (1-drop_mask) + drop_mask * T.nnet.sigmoid(dbm.bias_vis)
+
+        H_hat = ip.infer_H_hat_one_sided(
+                    other_H_hat = X_hat,
+                    W = vhW,
+                    b = dbm.bias_hid[0])
+
+        history = []
+
+        def update_history():
+            history.append( { 'X_hat' : X_hat, 'H_hat' : H_hat } )
+
+        update_history()
+
+        for i in xrange(self.n_iter-1):
+            H_hat = ip.infer_H_hat_one_sided(
+                    other_H_hat = X_hat,
+                    W = vhW,
+                    b = dbm.bias_hid[0])
+
+            unmasked = ip.infer_H_hat_one_sided(
+                        other_H_hat = H_hat,
+                        W = dbm.W[0].T,
+                        b = dbm.bias_vis)
+
+            X_hat = X * (1-drop_mask)+drop_mask * unmasked
+
+            update_history()
+
+
+        arg_to_sigmoid = T.dot(H_hat, dbm.W[0].T) + dbm.bias_vis
+        #cross entropy is
+        # - ( x log y + (1-x) log (1-y) )
+        # - ( x log sigmoid( z) + (1-x) log sigmoid ( - z) )
+        # x softplus( - z ) + (1-x) softplus ( z )
+
+        unmasked_cost = X * T.nnet.softplus( - arg_to_sigmoid) + (1.-X) * T.nnet.softplus( arg_to_sigmoid)
+
+        masked_cost = drop_mask * unmasked_cost
+
+        if self.reweight:
+            #this gives equal weight to each example
+            ave_cost = masked_cost.sum() / drop_mask.sum()
+        elif self.reweight_correctly:
+            ave_cost = masked_cost.sum(axis=1) / T.maximum(1,drop_mask.sum(axis=1))
+            ave_cost = ave_cost.mean()
+        else:
+            #this gives equal weight to each pixel, like in the pseudolikelihood cost
+            ave_cost = masked_cost.mean()
+
+        if not hasattr(self, 'inpaint_penalty'):
+                self.inpaint_penalty = 1.0
+
+        if self.h_target is not None and self.h_target != 0.0:
+            ave_cost = ave_cost + \
+                    self.h_penalty * abs(H_hat.mean(axis=0) - self.h_target).mean()
+
+        if self.weight_decay is not None and self.weight_decay != 0.0:
+            for W in model.W:
+                ave_cost = ave_cost + \
+                    self.weight_decay * T.sqr(W).mean()
+
+        if return_locals:
+            return locals()
+
+        return ave_cost
+
+def expected_energy(V_hat, H_hat, model):
+    v_bias = - T.dot(V_hat, model.bias_vis)
+    assert v_bias.ndim == 1
+    assert H_hat.ndim == 2
+    h_bias = - T.dot(H_hat, model.bias_hid[0])
+    assert h_bias.ndim == 1
+    weights = (T.dot(V_hat, model.W[0]) * H_hat).sum(axis=1)
+    assert weights.ndim == 1
+    rval = v_bias + h_bias + weights
+    assert rval.ndim == 1
+    return rval
+
+def H_entropy_term(H_hat):
+    z , = H_hat.owner.inputs
+    log_H_hat = -T.nnet.softplus(-z)
+    one_minus_H_hat = 1 - H_hat
+    log_one_minus_H_hat = -T.nnet.softplus(z)
+
+    rval = H_hat * log_H_hat + one_minus_H_hat * log_one_minus_H_hat
+    rval = rval.sum(axis=1)
+    assert rval.ndim == 1
+    return rval
+
+def V_entropy_term(unmasked, mask):
+    z ,= unmasked.owner.inputs
+
+    H_hat = unmasked
+    log_H_hat = -T.nnet.softplus(-z)
+    one_minus_H_hat = 1 - H_hat
+    log_one_minus_H_hat = -T.nnet.softplus(z)
+
+    rval = H_hat * log_H_hat + one_minus_H_hat * log_one_minus_H_hat
+    assert rval.ndim == 2
+    rval = (rval * mask).sum(axis=1)
+    assert rval.ndim == 1
+    return rval
+
+class RBM_Bad_Variational(UnsupervisedCost):
+    def __init__(self,
+                    n_iter,
+                    mask_gen = None,
+                    weight_decay = None,
+                    balance = False,
+                    reweight = True,
+                    reweight_correctly = False,
+                    h_target = None,
+                    h_penalty = None,
+                    ):
+        self.__dict__.update(locals())
+        del self.self
+        assert not (reweight and reweight_correctly)
+
+
+    def get_monitoring_channels(self, model, X, Y = None, drop_mask = None):
+
+        d = self(model, X = X, drop_mask = drop_mask, return_locals = True)
+
+        main_H_hat = d['main_H_hat']
+        H_hat_1 = d['H_hat_1']
+        H_hat_2 = d['H_hat_2']
+        rval = {}
+
+        def add_H_channels(H_hat, prefix):
+            h = H_hat.mean(axis=0)
+
+            rval[prefix+'h_min'] = h.min()
+            rval[prefix+'h_mean'] = h.mean()
+            rval[prefix+'h_max'] = h.max()
+
+            h_max = H_hat.max(axis=0)
+            h_min = H_hat.min(axis=0)
+
+            h_range = h_max - h_min
+
+            rval[prefix+'h_range_min'] = h_range.min()
+            rval[prefix+'h_range_mean'] = h_range.mean()
+            rval[prefix+'h_range_max'] = h_range.max()
+
+        add_H_channels(main_H_hat,'main_')
+        add_H_channels(H_hat_1,'Q1_')
+        add_H_channels(H_hat_2,'Q2_')
+
+        return rval
+
+    def __call__(self, model, X, drop_mask = None, return_locals = False):
+
+        assert not hasattr(model, 'beta') # this is only for binary for now
+        if not hasattr(model,'cost'):
+            model.cost = self
+        if not hasattr(model,'mask_gen'):
+            model.mask_gen = self.mask_gen
+
+        dbm = model
+        assert len(dbm.rbms) == 1
+
+        ip = dbm.inference_procedure
+
+        if drop_mask is None:
+            drop_mask = self.mask_gen(X)
+        else:
+            assert self.mask_gen is None
+
+        vhW = dbm.W[0]
+
+
+        main_H_hat = ip.infer_H_hat_one_sided(
+                    other_H_hat = X,
+                    W = vhW,
+                    b = dbm.bias_hid[0])
+
+        X_hat_1 = X * (1-drop_mask) + drop_mask * T.nnet.sigmoid(dbm.bias_vis)
+        X_hat_2 = X * drop_mask + (1-drop_mask) * T.nnet.sigmoid(dbm.bias_vis)
+
+        H_hat_1 = ip.infer_H_hat_one_sided(
+                    other_H_hat = X_hat_1,
+                    W = vhW,
+                    b = dbm.bias_hid[0])
+        H_hat_2 = ip.infer_H_hat_one_sided(
+                    other_H_hat = X_hat_2,
+                    W = vhW,
+                    b = dbm.bias_hid[0])
+
+        history = []
+
+        def update_history():
+            history.append( { 'X_hat' : X_hat_1, 'H_hat' : H_hat_1 } )
+
+        update_history()
+
+        for i in xrange(self.n_iter-1):
+            H_hat_1 = ip.infer_H_hat_one_sided(
+                        other_H_hat = X_hat_1,
+                        W = vhW,
+                        b = dbm.bias_hid[0])
+            H_hat_2 = ip.infer_H_hat_one_sided(
+                        other_H_hat = X_hat_2,
+                        W = vhW,
+                        b = dbm.bias_hid[0])
+
+
+            unmasked_1 = ip.infer_H_hat_one_sided(
+                        other_H_hat = H_hat_1,
+                        W = dbm.W[0].T,
+                        b = dbm.bias_vis)
+
+            X_hat_1 = X * (1-drop_mask)+drop_mask * unmasked_1
+
+            unmasked_2 = ip.infer_H_hat_one_sided(
+                        other_H_hat = H_hat_2,
+                        W = dbm.W[0].T,
+                        b = dbm.bias_vis)
+
+            X_hat_2 = X * drop_mask+(1-drop_mask) * unmasked_2
+
+            update_history()
+
+
+
+        class ElemwiseNoGradient(theano.tensor.Elemwise):
+
+            def connection_pattern(self, node):
+
+                return [ [ False ] ]
+
+            def grad(self, inputs, output_gradients):
+                return [ theano.gradient.DisconnectedType()() ]
+
+        block_gradient = ElemwiseNoGradient(theano.scalar.identity)
+        main_H_hat = block_gradient(main_H_hat)
+        H_hat_1 = block_gradient(H_hat_1)
+        H_hat_2 = block_gradient(H_hat_2)
+
+
+        main_energy = expected_energy(X, main_H_hat, model)
+        assert main_energy.ndim == 1
+        denom_energy_1 = expected_energy(X_hat_1, H_hat_1, model)
+        assert denom_energy_1.ndim == 1
+        denom_energy_2 = expected_energy(X_hat_2, H_hat_2, model)
+        assert denom_energy_2.ndim == 1
+
+        energy_terms = 2. * main_energy - denom_energy_1 - denom_energy_2
+        assert energy_terms.ndim == 1
+
+
+
+        entropy_terms = 2. * H_entropy_term(main_H_hat) - H_entropy_term(H_hat_1) - H_entropy_term(H_hat_2) \
+                - V_entropy_term(unmasked_1, drop_mask) - V_entropy_term(unmasked_2, 1-drop_mask)
+        assert entropy_terms.ndim == 1
+
+        total_cost = energy_terms + entropy_terms
+        assert total_cost.ndim == 1
+
+        ave_cost = total_cost.mean()
+
+
+        if self.h_target is not None and self.h_target != 0.0:
+            ave_cost = ave_cost + \
+                    self.h_penalty * abs(main_H_hat.mean(axis=0) - self.h_target).mean()
+
+        if self.weight_decay is not None and self.weight_decay != 0.0:
+            for W in model.W:
+                ave_cost = ave_cost + \
+                    self.weight_decay * T.sqr(W).mean()
+
+        if return_locals:
+            return locals()
+
+        return ave_cost
+
+
 class MaskGen:
     def __init__(self, drop_prob, balance, n_channels = 1):
         self.__dict__.update(locals())
