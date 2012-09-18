95c2d64d4765765e81b4e234baa03d8a7aa53814
Ian Goodfellow <goodfellow.ian@gmail.com>
2012-09-18 11:33:33 -0400

    finished implementing main show_super_samples script (doesn't work yet because classes don't have the functionality it calls)

--- dbm/inpaint/show_super_samples.py
+++ dbm/inpaint/show_super_samples.py
@@ -2,6 +2,9 @@ from pylearn2.utils import serial
 import sys
 from pylearn2.config import yaml_parse
 from pylearn2.gui.patch_viewer import PatchViewer
+import time
+from theano import function
+from theano.sandbox.rng_mrg import MRG_RandomStreams
 
 _, model_path = sys.argv
 
@@ -37,4 +40,34 @@ show()
 # Make shared variables representing the sampling state of the model
 layer_to_state = model.make_layer_to_state(m)
 # Seed the sampling with the data batch
-layer_to_state[model.visible_layer].set_value(vis_batch)
+vis_sample = layer_to_state[model.visible_layer]
+vis_sample.set_value(vis_batch)
+
+theano_rng = MRG_RandomStreams(2012+9+18)
+
+sampling_updates = model.get_sampling_updates(layer_to_state, theano_rng)
+
+t1 = time.time()
+sample_func = function([], updates=sampling_updates)
+t2 = time.time()
+
+print 'Sampling function compilation took',t2-t1
+
+while True:
+    print 'Displaying samples. How many steps to take next? (q to quit)'
+    while True:
+        x = raw_input()
+        if x == 'q':
+            quit()
+        try:
+            x = int(x)
+            break
+        except:
+            print 'Invalid input, try again'
+
+    for i in xrange(x):
+        sample_func()
+
+    vis_batch = vis_sample.get_value()
+
+
