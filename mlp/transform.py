5ca729582a337ebbc19c60c67d57684ef698a8a7
Ian Goodfellow <goodfellow.ian@gmail.com>
2013-02-05 14:45:50 -0500

    fixed bug in log space params

--- mlp/transform.py
+++ mlp/transform.py
@@ -6,7 +6,7 @@ class pow10(object):
         self.offset = offset
 
     def inverse(self, x):
-        return np.log(x - self.offset) / np.exp(10.)
+        return np.log(x - self.offset) / np.log(10.)
 
     def __call__(self, x):
         return self.offset + 10. ** x
