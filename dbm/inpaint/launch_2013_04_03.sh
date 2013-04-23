1589a831d9a5e0c87cfd462c10f61b1fa00be6fa
Ian Goodfellow <goodfellow.ian@gmail.com>
2013-04-23 15:27:50 -0400

    config costs.dbm

---
+++ dbm/inpaint/launch_2013_04_03.sh
@@ -0,0 +1,1 @@
+jobdispatch --torque --env=THEANO_FLAGS=device=gpu,floatX=float32,force_device=True --duree=48:00:00 --whitespace --gpu train.py $G/dbm/inpaint/expdir/mnist_sup_inpaint_W8"{{B,C,D,E,F,G,H,I,J,K}}".yaml
