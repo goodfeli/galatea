86ed7a15f91fbed02e3516ee449ea7aec97410a7
Jason Yosinski <jason@yosinski.com>
2014-01-10 17:50:43 -0500

    working on iterator

--- fish/datasets/wiskott.py
+++ fish/datasets/wiskott.py
@@ -24,17 +24,19 @@ class WiskottVideoConfig(object):
     with identical configuration using anchors in YAML files.
     '''
 
-    def __init__(self, axes = ('b', 0, 1, 2, 'c'),
-                 num_frames = 3, height = 32, width = 32, num_channels = 1):
+    def __init__(self, axes = ('c', 0, 1, 'b'),
+                 num_frames = 3,
+                 #height = 32, width = 32,
+                 num_channels = 1):
         # Arbitrary choice: we do the validation here, not in WiskottVideo
         assert isinstance(axes, tuple), 'axes must be a tuple'
         self.axes = axes
         assert num_frames > 0, 'num_frames must be positive'
         self.num_frames = num_frames
-        assert height > 0, 'height must be positive'
-        self.height = height
-        assert width > 0, 'width must be positive'
-        self.width = width
+        #assert height > 0, 'height must be positive'
+        #self.height = height
+        #assert width > 0, 'width must be positive'
+        #self.width = width
         assert num_channels == 1, 'only 1 channel is supported for now'
         self.num_channels = num_channels
 
@@ -66,8 +68,8 @@ class WiskottVideo(Dataset):
         # Copy main config from provided config
         self.axes            = config.axes
         self.num_frames      = config.num_frames
-        self.height          = config.height
-        self.width           = config.width
+        #self.height          = config.height
+        #self.width           = config.width
         self.num_channels    = config.num_channels
 
         # Load data into memory
@@ -88,6 +90,23 @@ class WiskottVideo(Dataset):
         assert len(self._feature_matrices) == len(self._label_matrices)
         self._n_matrices = len(self._feature_matrices)
 
+        print 'Memory used for features/labels: %.3fG/%.3fG' % (
+            sum([mat.nbytes for mat in self._feature_matrices]) / 1.0e9,
+            sum([mat.nbytes for mat in self._label_matrices]) / 1.0e9
+            )
+
+
+        
+        print 'TODO'
+        return
+        self.__space = CompositeSpace((
+            Conv3DSpace(TODO),
+            VectorSpace(dim = TODO)))
+        self.source = ('features', 'labels')
+        self.data_specs = (self.__space, self.soruce)
+
+
+        
     def _load_data(self, data_directories, file_regex, is_labels=False):
         filenames = []
         for data_directory in data_directories:
@@ -121,9 +140,12 @@ class WiskottVideo(Dataset):
             if is_labels:
                 assert ('fish' in filename) or ('spheres' in filename), 'Not sure if fish or spheres.'
                 is_fish = 'fish' in filename
-                matrices.append(load_labels(filename, is_fish))
+                mat = load_labels(filename, is_fish)   # e.g (201,16)
+                matrices.append(mat)
             else:
-                matrices.append(serial.load(filename))
+                mat = serial.load(filename)    # e.g (156,156,201)
+                mat = np.array(np.rollaxis(mat, 2, 0), copy=True)   # e.g (201,156,156)
+                matrices.append(mat)
             print '\b\b\b\b\b\b%5d' % (ii+1),
             sys.stdout.flush()
         print
@@ -131,24 +153,34 @@ class WiskottVideo(Dataset):
 
 
     @functools.wraps(Dataset.iterator)
-    def OLD_iterator(self, mode=None, batch_size=None, num_batches=None,
+    def iterator(self, mode=None, batch_size=None, num_batches=None,
                  topo=None, targets=None, rng=None, data_specs=None,
                  return_tuple=False, ignore_data_specs=False):
 
-        if batch_size is None: batch_size = 100
-        if num_batches is None: num_batches = 100
+        # The batch_size contains the "unrolled" size, since we're returning a Conv2DSpace.
+        if batch_size is None: batch_size = 10 * self.num_frames
+        if num_batches is None: num_batches = 20
         assert batch_size > 0
+        assert batch_size % self.num_frames == 0, (
+            'Iterator must be created with batch_size = num_frames * an integer'
+            )
+        slices_per_batch = batch_size / self.num_frames
         assert num_batches > 0
         assert topo is None
         assert targets is None
 
+        assert mode is None        # TODO: later...
         if mode is None: mode = 'shuffled_sequential'
-        assert mode in ('sequential', 'shuffled_sequential'), (
-            'Mode must be one of: sequential, shuffled_sequential'
-        )
-        if mode != 'shuffled_sequential':
-            warnings.warn('billiard dataset returning its only supported iterator type -- shuffled -- despite the request to the contrary')
-        if not ignore_data_specs:
+        #assert mode in ('sequential', 'shuffled_sequential'), (
+        #    'Mode must be one of: sequential, shuffled_sequential'
+        #)
+        #if mode != 'shuffled_sequential':
+        #    warnings.warn('billiard dataset returning its only supported iterator type -- shuffled -- despite the request to the contrary')
+
+        print 'Got these data_specs:'
+        print data_specs
+        #if not ignore_data_specs:
+        if False:
             assert data_specs != None, 'Must provide data_specs'
             assert len(data_specs) == 2, 'data_specs must include only one tuple for "features"'
             assert type(data_specs[0]) is CompositeSpace, 'must be composite space...??'
@@ -157,6 +189,18 @@ class WiskottVideo(Dataset):
                 'data_specs must include only one tuple for "features"'
             )
 
+        if not hasattr(rng, 'random_integers'):
+            rng = np.random.RandomState(rng)
+
+        return MultiplexingMatrixIterator(
+            self._feature_matrices,
+            self._label_matrices,
+            num_batches = num_batches,
+            num_slices = slices_per_batch,
+            slice_length = self.num_frames,
+            rng = rng,
+            )
+            
         #underlying_dataspecs = (self._output_space, 'features')
         underlying_space = Conv2DSpace((self.height, self.width),
                                        num_channels = self.num_channels)
@@ -178,6 +222,114 @@ class WiskottVideo(Dataset):
             num_concat = batch_size,
             return_tuple = return_tuple
         )
+
+
+
+class MultiplexingMatrixIterator(object):
+    '''An iterator that creates samples by randomly drawing from
+    ...
+
+    Hardcoded: sequential blocks from each matrix
+
+    TODO: write
+    '''
+
+    def __init__(self, list_features, list_labels,
+                 num_slices, slice_length, num_batches,
+                 rng = None, return_tuple = False):
+        '''
+        num_slices: number of slices to take. Each slice is from a different
+        randomly selected matrix (with replacement).
+        
+        slice_length: how long each slice is within a list.
+        
+        For example, with num_slices = 5 and slice_length = 3, we would randomly
+        choose 5 matrices and concatenate a slice of length 3 from each matrix for a
+        total returned length of 15.
+        '''
+        #self._underlying_iterator = underlying_iterator
+        #assert num_concat > 0, 'num_concat must be positive'
+        #self._num_concat = num_concat
+        self._return_tuple = return_tuple
+
+        self.list_features = list_features
+        self.list_labels   = list_labels
+        assert len(list_features) == len(list_labels), 'list length mismatch'
+        self.n_lists = len(list_features)
+        assert num_slices > 0
+        assert slice_length > 0
+        for m1,m2 in zip(self.list_features, self.list_labels):
+            assert m1.shape[0] == m2.shape[0], 'matrix leading dimensions must match'
+            assert m1.shape[0] >= slice_length, (
+                'matrix of size (%d,...) not long enough for slice of length %d' % (m1.shape[0], slice_length)
+                )
+        self._num_slices = num_slices
+        self._slice_length = slice_length
+
+        # Never used except to return our num_batches
+        assert num_batches > 0
+        self._num_batches = num_batches
+        
+        # Compute max starting indices and probability for all lists
+        self.max_start_idxs = [mm.shape[0] - self._slice_length for mm in self.list_features]
+        probs = [mm+1 for mm in self.max_start_idxs]  # proportional to number of possible windows
+        self.probabilities = np.array(probs, dtype=float)
+        self.probabilities /= float(sum(self.probabilities))
+        assert sum(self.probabilities) == 1.0
+        
+        if hasattr(rng, 'random_integers'):
+            self.rng = rng
+        else:
+            self.rng = np.random.RandomState(rng)
+        
+    def __iter__(self):
+        return self
+
+    def next(self):
+        for ii in xrange(self._num_slices):
+            #listId = self.rng.choice(len(nFrames), 1, p = self.probabilities)[0]
+            # Inefficient hack because Montreal's version of numpy is old
+            list_idx = np.argwhere(self.rng.multinomial(1, self.probabilities))[0,0]
+            slice_start = self.rng.randint(0, self.max_start_idxs[list_idx])
+
+            print 'TODO here: check right shape?'
+            print 'TODO here: which of {features,labels,both} do we return?'
+            #pdb.set_trace()
+            block = self.list_features[list_idx][:,:,slice_start:(slice_start+self._slice_length)]  # TODO: likely wrong!
+
+            if ii == 0:
+                out = np.zeros((self._num_slices,) + block.shape,
+                               dtype = block.dtype)
+            out[ii] = block
+
+        # Flatten out by one dimension before returning
+        print 'TODO: check dims here! Add this part!'
+        ret = out
+        #ret = reshape(out, ... DO THIS )
+
+        if self._return_tuple:
+            return (ret,)
+        else:
+            return ret
+
+    @property
+    def batch_size(self):
+        ret = self._slice_length * self._num_slices
+        return ret
+    
+    @property
+    def num_batches(self):
+        return self._num_batches
+    
+    @property
+    def num_examples(self):
+        return self.batch_size * self.num_batches
+    
+    @property
+    def stochastic(self):
+        return True
+
+
 
 def load_labels(path, is_fish):
     """
@@ -243,7 +395,9 @@ def load_labels(path, is_fish):
 
     return rval
 
-class WiskottVideo2(VideoDataset):
+
+
+class WiskottVideo2_DEPRECATED(VideoDataset):
 
     def __init__(self, config):
         data =  WiskottVideo('train', config, quick = False)
@@ -261,25 +415,30 @@ class WiskottVideo2(VideoDataset):
         self.source = ('features', 'labels')
         self.data_specs = (self.space, self.soruce)
 
+
+
 def demo():
-    num_frames = 300
-    height = 10
-    width  = 10
+    num_frames = 5
+    #height = 10
+    #width  = 10
 
     config = WiskottVideoConfig(
         num_frames = num_frames,
-        height = height,
-        width  = width,
+        #height = height,
+        #width  = width,
         )
 
-    #wisk = WiskottVideo2('train', config, quick = True)
-    wisk = WiskottVideo2(config)
-    pdb.set_trace()
+    wisk = WiskottVideo('train', config, quick = True)
+    #pdb.set_trace()
 
     it = wisk.iterator()
+    example = it.next()
 
+    print 'got example of shape:', example.shape
+    
     print 'done.'
-    pdb .set_trace()
+    pdb.set_trace()
+
 
 
 if __name__ == '__main__':
