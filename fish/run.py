3e1dc525e61c5693b7231d0126a0fe6eea663412
Jason Yosinski <jason@yosinski.com>
2014-01-20 13:58:41 -0500

    Updated run script

--- fish/run.py
+++ fish/run.py
@@ -4,18 +4,17 @@ import sys, os
 
 os.environ['THEANO_FLAGS'] = 'device=gpu,floatX=float32'
 
-# Fake running jtrain.py:
-#sys.argv.append('fish_model_ccn.yaml')
-class Container(): pass
-
-args = Container
-args.config = 'fish_model_ccn.yaml'
-args.name = 'junk'
-args.diary = False
-args.quick = True
-args.timestamp = False
-args.verbose_logging = False
-args.debug = False
+## Fake running jtrain.py:
+##sys.argv.append('fish_model_ccn.yaml')
+#class Container(): pass
+#args = Container
+#args.config = 'fish_model_ccn.yaml'
+#args.name = 'junk'
+#args.diary = False
+#args.quick = True
+#args.timestamp = False
+#args.verbose_logging = False
+#args.debug = False
 
 from pylearn2.scripts.jtrain import *
-main2(args)
+main('--quick fish_model_ccn.yaml'.split())
