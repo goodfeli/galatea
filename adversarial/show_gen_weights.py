49fd38a4242e36a1806bf4a1cfbb734086aa6159
Ian Goodfellow <goodfellow.ian@gmail.com>
2014-05-29 17:22:16 -0400

    fixed bug in show_gen_weights

--- adversarial/show_gen_weights.py
+++ adversarial/show_gen_weights.py
@@ -3,10 +3,12 @@ from pylearn2.utils import serial
 model = serial.load(sys.argv[1])
 generator = model.generator
 i = -1
-while True:
+success = False
+while not success:
     final = generator.mlp.layers[i]
     try:
         weights = final.get_weights()
+        success = True
     except NotImplementedError:
         i -= 1 # skip over SpaceConverter, etc.
 
