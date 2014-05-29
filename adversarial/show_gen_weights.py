68456e27e4c14da163b8b24d86455e4bf82a2b52
Ian Goodfellow <goodfellow.ian@gmail.com>
2014-05-29 16:37:11 -0400

    made show_gen_weights work with SpaceConverter

--- adversarial/show_gen_weights.py
+++ adversarial/show_gen_weights.py
@@ -2,7 +2,14 @@ import sys
 from pylearn2.utils import serial
 model = serial.load(sys.argv[1])
 generator = model.generator
-final = generator.mlp.layers[-1]
-weights = final.get_weights()
+i = -1
+while True:
+    final = generator.mlp.layers[i]
+    try:
+        weights = final.get_weights()
+    except NotImplementedError:
+        i -= 1 # skip over SpaceConverter, etc.
+
+
 from pylearn2.gui.patch_viewer import make_viewer
 make_viewer(weights, is_color=weights.shape[1] % 3 == 0).show()
