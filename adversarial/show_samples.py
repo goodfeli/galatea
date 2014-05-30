f81288160e1be0fcfe0f1681ec506f929fc5ecff
Ian Goodfellow <goodfellow.ian@gmail.com>
2014-05-30 15:46:14 -0400

    show nearest traine xample

--- adversarial/show_samples.py
+++ adversarial/show_samples.py
@@ -8,19 +8,26 @@ from pylearn2.space import VectorSpace
 from pylearn2.config import yaml_parse
 import numpy as np
 
-match_train = False
+match_train = True
 if match_train:
     dataset = yaml_parse.load(model.dataset_yaml_src)
+
+grid_shape = None
 
 if isinstance(space, VectorSpace):
     # For some reason format_as from VectorSpace is not working right
     samples = model.generator.sample(100).eval()
 
     if match_train:
+        grid_shape = (10, 20)
         matched = np.zeros((samples.shape[0] * 2, samples.shape[1]))
+        X = dataset.X
         for i in xrange(samples.shape[0]):
             matched[2 * i, :] = samples[i, :].copy()
-        raise NotImplementedError("finish this")
+            dists = np.square(X - samples[i, :]).sum(axis=1)
+            j = np.argmin(dists)
+            matched[2 * i + 1, :] = X[j, :]
+        samples = matched
 
     is_color = samples.shape[-1] % 3 == 0
 else:
@@ -39,5 +46,5 @@ print (samples.min(), samples.mean(), samples.max())
 # Hack for detecting MNIST [0, 1] values. Otherwise we assume centered images
 if samples.min() >0:
     samples = samples * 2.0 - 1.0
-viewer = make_viewer(samples, is_color=is_color)
+viewer = make_viewer(samples, grid_shape=grid_shape, is_color=is_color)
 viewer.show()
