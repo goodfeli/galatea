c487862aad515551a1de00601b3be6992a813b23
Ian Goodfellow <goodfellow.ian@gmail.com>
2014-05-30 07:14:31 -0400

    better biconv hyperparams

--- adversarial/show_gen_weights.py
+++ adversarial/show_gen_weights.py
@@ -1,7 +1,21 @@
 import sys
+from pylearn2.gui.patch_viewer import make_viewer
 from pylearn2.utils import serial
 model = serial.load(sys.argv[1])
 generator = model.generator
+
+final = generator.mlp.layers[-1]
+success = False
+try:
+    topo = final.get_weights_topo()
+    success = True
+except Exception:
+    pass
+
+if success:
+    make_viewer(topo).show()
+    quit()
+
 i = -1
 success = False
 while not success:
@@ -13,5 +27,4 @@ while not success:
         i -= 1 # skip over SpaceConverter, etc.
 
 
-from pylearn2.gui.patch_viewer import make_viewer
 make_viewer(weights, is_color=weights.shape[1] % 3 == 0).show()
